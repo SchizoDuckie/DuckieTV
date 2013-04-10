7eebecd0f0767b35a6637a3253baf4bc52842877
Phil Rennie <phil@philrennie.co.uk>
2013-04-10 23:44:22 +0100

    CSS transitions are broken on Chrome 26 on Mac. Detect browser and override styles to remove transitions.

--- app/popup.js
+++ app/popup.js
@@ -1,3 +1,9 @@
+// css transitions are broken in chrome 26 on mac
+// add css overrides if appropriate
+if (navigator.appVersion.indexOf("Mac") != -1 && navigator.appVersion.indexOf("Chrome/26") != -1) {
+    document.write('<link rel="stylesheet" href="static/css/osxstyle.css" />');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     
     window.tvDB = new tvDB();
