6e814d1768ee85c47b60456704f2dd5a898f9610
Jelle Ursem <SchizoDuckie@gmail.com>
2014-04-27 00:33:28 +0200

    Move serieslist related stuff into serieslist directive

--- js/directives/seriesList.js
+++ js/directives/seriesList.js
@@ -127,9 +127,32 @@ angular.module('DuckieTV.directives.serieslist', [])
                         $rootScope.$broadcast('storage:update');
                     });
                 });
-
                 $scope.selected = '';
             }
+
+            $scope.favorites = [];
+            $scope.searchEngine = 1;
+
+
+            /**
+             * The favorites service fetches data asynchronously via SQLite, we wait for it to emit the favorites:updated event.
+             */
+            $scope.favorites = FavoritesService.favorites;
+            $rootScope.$on('favorites:updated', function(event, data) {
+                // you could inspect the data to see if what you care about changed, or just update your own scope
+                if (FavoritesService.favorites != $scope.favorites) $scope.favorites = FavoritesService.favorites;
+                if (!$scope.favorites || (FavoritesService.favorites && FavoritesService.favorites.length == 0)) {
+                    $rootScope.$broadcast('serieslist:empty');
+                } else {
+                    var serie = $scope.favorites[Math.floor(Math.random() * $scope.favorites.length)];
+                    $rootScope.$broadcast('background:load', serie.fanart);
+                }
+            });
+            $rootScope.$on('episodes:inserted', function(event, serie) {
+                if (serie.get('fanart') != null) {
+                    $rootScope.$broadcast('background:load', serie.get('fanart'));
+                }
+            });
 
             $rootScope.$on('serieslist:empty', function() {
                 $scope.activate();
