67dbbbb53700bc7ba91c88465304b8c8074d2ace
Jelle Ursem <SchizoDuckie@gmail.com>
2014-05-03 14:08:09 +0200

    Cleanup torrent client for autoconnect

--- js/controllers/TorrentCtrl.js
+++ js/controllers/TorrentCtrl.js
@@ -26,11 +26,6 @@ angular.module('DuckieTorrent.controllers', ['DuckieTorrent.torrent', 'DuckieTV.
             return 7 * Math.pow(i, 3) + 3 * Math.pow(i, 2) + 5 * i + 10000;
         }
 
-        $scope.Cast = function() {
-            console.log('connecting!');
-            DuckieTVCast.initialize();
-        }
-
         $scope.AutoConnect = function() {
             uTorrent.AutoConnect().then(function(rpc) {
                 $scope.rpc = rpc;
@@ -55,67 +50,8 @@ angular.module('DuckieTorrent.controllers', ['DuckieTorrent.torrent', 'DuckieTV.
         }
 
 
-        discoverLocalIP = function() {
-            console.log("Discover local IP! ");
-            var p = $q.defer();
-
-            var RTCPeerConnection = window.webkitRTCPeerConnection;
-            var addrs = {};
-
-            var rtc = new RTCPeerConnection({
-                iceServers: []
-            });
-            if (window.mozRTCPeerConnection) rtc.createDataChannel('', {
-                reliable: false
-            });
-
-            function grepSDP(sdp) {
-                var hosts = [];
-                sdp.split('\r\n').map(function(line) {
-                    if (~line.indexOf('a=candidate')) {
-                        var parts = line.split(' '),
-                            addr = parts[4],
-                            type = parts[7];
-                        if (type === 'host') addrs[addr] = true;
-                    } else if (~line.indexOf('c=')) {
-                        //  var parts = line.split(' '),
-                        //     addr = parts[2];
-                        //  addrs[addr] = true;
-                    }
-                });
-            }
-
-            rtc.onicecandidate = function(evt) {
-                if (evt.candidate) {
-                    grepSDP(evt.candidate.candidate);
-                }
-            }
-
-            rtc.createOffer(function(desc) {
-                rtc.setLocalDescription(desc)
-            }, function(e) {});
-
-            setTimeout(function() {
-                p.resolve(Object.keys(addrs));
-            }, 1500);
-
-            return p.promise;
-
-        }
-
-        $scope.getLocalIP = function() {
-            discoverLocalIP().then(function(result) {
-                console.debug("Local IP Address: ", result);
-                $scope.localIpAddresses = result;
-            });
-        };
-
         $scope.localIpAddress = $rootScope.getSetting('ChromeCast.localIpAddress');
 
-        $scope.setStreamingSource = function(address) {
-            $rootScope.setSetting('ChromeCast.localIpAddress', address);
-            $scope.localIpAddress = address;
-        }
 
 
     })
