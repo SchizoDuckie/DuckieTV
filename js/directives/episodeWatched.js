98a3b1cf35110bf5bf47258d674970fe12a8ba75
garfield69 <garfield69@outlook.com>
2014-06-26 06:53:47 +1200

    Update episodeWatched.js

--- js/directives/episodeWatched.js
+++ js/directives/episodeWatched.js
@@ -1,13 +1,13 @@
 angular.module('DuckieTV.directives.episodewatched', [])
 
-.directive('episodeWatched', function($rootScope, $document) {
+.directive('episodeWatched', function($rootScope, $filter, $document) {
     return {
         restrict: 'E',
         transclude: true,
         scope: {
             episode: '='
         },
-        template: ['<a ng-click="markWatched()" class="glyphicon" tooltip="{{getToolTip()|translate}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ==  1, \'glyphicon-eye-close\' : episode.get(\'watched\') != 1 }" ng-transclude></a>'],
+        template: ['<a ng-click="markWatched()" class="glyphicon" tooltip="{{getToolTip()|translate}}{{getToolTipDate()}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ==  1, \'glyphicon-eye-close\' : episode.get(\'watched\') != 1 }" ng-transclude></a>'],
         link: function($scope) {
 
             $scope.tooltip = null;
@@ -15,6 +15,9 @@ angular.module('DuckieTV.directives.episodewatched', [])
             $scope.getToolTip = function() {
                 return $scope.episode.get('watched') == 1 ? 'EPISODEWATCHED_JS-is-marked-lbl' : 'EPISODEWATCHED_JS-not-marked-lbl';
             }
+            $scope.getToolTipDate = function() {
+                return $scope.episode.get('watched') == 1 ? $filter('date')(new Date($scope.episode.get('watchedAt')), 'medium') : null;
+           }
             $scope.markWatched = function() {
 
                 if ($scope.episode.get('watched') == '1') {
