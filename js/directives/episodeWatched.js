f65f8604e5d6cad0b64c305d83199b29691c18ed
garfield69 <garfield69@outlook.com>
2014-06-25 17:26:00 +1200

    Update episodeWatched.js

--- js/directives/episodeWatched.js
+++ js/directives/episodeWatched.js
@@ -7,13 +7,13 @@ angular.module('DuckieTV.directives.episodewatched', [])
         scope: {
             episode: '='
         },
-        template: ['<a ng-click="markWatched()" class="glyphicon" tooltip="{{getToolTip()}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ==  1, \'glyphicon-eye-close\' : episode.get(\'watched\') != 1 }" ng-transclude></a>'],
+        template: ['<a ng-click="markWatched()" class="glyphicon" tooltip="{{getToolTip()|translate}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ==  1, \'glyphicon-eye-close\' : episode.get(\'watched\') != 1 }" ng-transclude></a>'],
         link: function($scope) {
 
             $scope.tooltip = null;
 
             $scope.getToolTip = function() {
-                return $scope.episode.get('watched') == 1 ? "You marked this episode as watched at " + new Date($scope.episode.get('watchedAt')).toLocaleString() : "Mark this episode as watched";
+                return $scope.episode.get('watched') == 1 ? 'EPISODEWATCHED_JS-is-marked-lbl' : 'EPISODEWATCHED_JS-not-marked-lbl';
             }
             $scope.markWatched = function() {
 
