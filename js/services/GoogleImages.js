8b9e2286917cfcacd01b69c694c9a4c7ed46e27f
garfield69 <garfield69@outlook.com>
2014-06-25 17:23:17 +1200

    Update GoogleImages.js

--- js/services/GoogleImages.js
+++ js/services/GoogleImages.js
@@ -106,7 +106,7 @@ angular.module('DuckieTV.providers.googleimages', [])
     return {
         restrict: 'E',
         template: ['<div ng-controller="FindIMDBTypeAheadCtrl">',
-            '<input type="text" ng-model="selected" placeholder="Search IMDB for movies or series"',
+            '<input type="text" ng-model="selected" placeholder="{{\'GOOGLEIMAGES_JS-placeholder-lbl\'|translate}}"',
             'typeahead-min-length="3" typeahead-loading="loadingIMDB"',
             'typeahead="result for results in find($viewValue)  | filter: orderBy: \'title\'" typeahead-template-url="templates/typeAheadIMDB.html"',
             'typeahead-on-select="selectIMDB($item)" class="form-control"> <i ng-show="loadingIMDB" class="glyphicon glyphicon-refresh"></i>',
