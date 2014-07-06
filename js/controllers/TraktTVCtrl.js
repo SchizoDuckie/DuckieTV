c53e958f7c0941955f1da14e9945334062192e6c
Jelle Ursem <SchizoDuckie@gmail.com>
2014-07-06 02:57:28 +0200

    Implemented trakt.tv reading favorite shows and displaying them with fetched episodes as serieHeader directive. re #27
    Todo: feed shows to FavoritesService.addFavorite and parse TraktTv.getUserWatched into episodes to mark as watched.

--- js/controllers/TraktTVCtrl.js
+++ js/controllers/TraktTVCtrl.js
@@ -6,13 +6,36 @@
          username: 'schizoduckie',
          password: null,
          passwordHash: null
-     }
+     };
+
+     $scope.traktTVSeries = null;
+     $scope.tvdbSeries = {};
 
      $scope.encryptPassword = function() {
          if ($scope.credentials.password !== null) {
              $scope.credentials.passwordHash = CryptoJS.SHA1($scope.credentials.password).toString();
              $scope.credentials.password = angular.copy($scope.credentials.passwordHash);
          }
+     }
+
+     $scope.isDownloaded = function(tvdb_id) {
+         return tvdb_id in $scope.tvdbSeries;
+     }
+
+     $scope.getDownloaded = function(tvdb_id) {
+         return $scope.tvdbSeries[tvdb_id];
+     }
+
+     $scope.readTraktTV = function() {
+         TraktTV.getUserShows($scope.credentials.username).then(function(data) {
+             console.log("Found series from Trakt.tV", data);
+             data.map(function(show) {
+                 TraktTV.findSerieByTVDBID(show.tvdb_id).then(function(show) {
+                     $scope.tvdbSeries[show.tvdb_id] = show;
+                 });
+             });
+             $scope.traktTVSeries = data;
+         });
      }
 
  });
