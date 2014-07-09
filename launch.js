8a76b1928a77d579c4c12959e8a5b579573799e4
garfield69 <garfield69@outlook.com>
2014-07-09 16:47:47 +1200

    no migrations in this version.

--- launch.js
+++ launch.js
@@ -3,7 +3,9 @@ chrome.runtime.onInstalled.addListener(function(details) {
     localStorage.setItem('runtime.event', angular.toJson(details, true));
     if (details.reason == "install") {
         console.log("This is a first install!");
-        localStorage.setItem('0.4migration', 'done');
+        /*
+         * example: localStorage.setItem('0.4migration', 'done');
+        */
     } else if (details.reason == "update") {
         var thisVersion = chrome.runtime.getManifest().version;
         console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
