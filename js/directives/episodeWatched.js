262dc76b18aadec01ce4c4e73c6d3b675bbf02eb
Garfield69 <garfield69@outlook.com>
2014-07-16 08:32:19 +1200

    make all episode.get('watched') comparisons against strings to remind us that 'watched' is a varChar not a boolean.

--- js/directives/episodeWatched.js
+++ js/directives/episodeWatched.js
@@ -11,7 +11,7 @@ angular.module('DuckieTV.directives.episodewatched', [])
         scope: {
             episode: '='
         },
-        template: ['<a ng-click="markWatched()" style="width:100%" class="glyphicon" tooltip="{{getToolTip()}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ==  1, \'glyphicon-eye-close\' : episode.get(\'watched\') != 1 }" ng-transclude></a>'],
+        template: ['<a ng-click="markWatched()" style="width:100%" class="glyphicon" tooltip="{{getToolTip()}}" ng-class="{ \'glyphicon-eye-open\' : episode.get(\'watched\') ===  \'1\', \'glyphicon-eye-close\' : episode.get(\'watched\') !== \'1\' }" ng-transclude></a>'],
         link: function($scope) {
 
             $scope.tooltip = null;
@@ -20,7 +20,7 @@ angular.module('DuckieTV.directives.episodewatched', [])
              * Translate the watchedAt tooltip
              */
             $scope.getToolTip = function() {
-                return $scope.episode.get('watched') == 1 ?
+                return $scope.episode.get('watched') === '1' ?
                     $filter('translate')('EPISODEWATCHEDjs/is-marked/lbl') +
                     $filter('date')(new Date($scope.episode.get('watchedAt')), 'medium') :
                     $filter('translate')('EPISODEWATCHEDjs/not-marked/lbl');
@@ -30,7 +30,7 @@ angular.module('DuckieTV.directives.episodewatched', [])
              * Pass the logic to the episode to handle marking watched in a generic way
              */
             $scope.markWatched = function() {
-                if ($scope.episode.get('watched') == '1') {
+                if ($scope.episode.get('watched') === '1') {
                     $scope.episode.markNotWatched($injector.get('$rootScope'));
                 } else {
                     $scope.episode.markWatched($injector.get('$rootScope'));
