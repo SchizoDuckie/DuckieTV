SchizoDuckie/DuckieTV

commit 972c52485e10507e1aba3f7487c1099d205445cb

remove debug console.logs from calendar.js

js/directives/calendar.js
@@ -137,8 +137,8 @@ angular.module('DuckieTV.directives.calendar', ['DuckieTV.providers.favorites'])
     $rootScope.$on('episode:marked:watched', function(event, data) {
         service.setEvents([{ 
             start: new Date(data.get('firstaired')),
-            serie: cache[data.get('ID_Serie')].get('name'),
-            serieID: cache[data.get('ID_Serie')].get('TVDB_ID'),
+            serie: data.get('ID_Serie').get('name'),
+            serieID: data.get('ID_Serie').get('TVDB_ID'),
             episodeID: data.get('TVDB_ID'),
         	episode: data.asObject() 
         }]);       
@@ -147,8 +147,8 @@ angular.module('DuckieTV.directives.calendar', ['DuckieTV.providers.favorites'])
     $rootScope.$on('episode:marked:notwatched', function(event, data) {
         service.setEvents([{ 
             start: new Date(data.get('firstaired')),
-            serie: cache[data.get('ID_Serie')].get('name'),
-            serieID: cache[data.get('ID_Serie')].get('TVDB_ID'),
+            serie: data.get('ID_Serie').get('name'),
+            serieID: data.get('ID_Serie').get('TVDB_ID'),
             episodeID: data.get('TVDB_ID'),
         	episode: data.asObject() 
         }]);       

commit 14afb4e6249c14b56b9c15f7357a432795e72248

fix for 972c5248 which caused "TypeError: undefined is not a function
    at chrome-extension://cdfkaloficjmdjbgmckaddgfcghgidei/js/directives/calendar.js:152:41"

js/directives/calendar.js
@@ -137,23 +137,31 @@ angular.module('DuckieTV.directives.calendar', ['DuckieTV.providers.favorites'])
     };
 
     $rootScope.$on('episode:marked:watched', function(event, data) {
-        service.setEvents([{ 
-            start: new Date(data.get('firstaired')),
-            serie: data.get('ID_Serie').get('name'),
-            serieID: data.get('ID_Serie').get('TVDB_ID'),
-            episodeID: data.get('TVDB_ID'),
-            episode: data.asObject() 
-        }]);       
+        CRUD.FindOne('Serie', {
+            ID_Serie: data.get('ID_Serie')
+        }).then(function(s) {
+            service.setEvents([{ 
+                start: new Date(data.get('firstaired')),
+                serie: s.get('name'),
+                serieID: s.get('TVDB_ID'),
+                episodeID: data.get('TVDB_ID'),
+                episode: data 
+            }]);
+        });
     });
 
     $rootScope.$on('episode:marked:notwatched', function(event, data) {
-        service.setEvents([{ 
-            start: new Date(data.get('firstaired')),
-            serie: data.get('ID_Serie').get('name'),
-            serieID: data.get('ID_Serie').get('TVDB_ID'),
-            episodeID: data.get('TVDB_ID'),
-            episode: data.asObject() 
-        }]);       
+        CRUD.FindOne('Serie', {
+            ID_Serie: data.get('ID_Serie')
+        }).then(function(s) {
+            service.setEvents([{ 
+                start: new Date(data.get('firstaired')),
+                serie: s.get('name'),
+                serieID: s.get('TVDB_ID'),
+                episodeID: data.get('TVDB_ID'),
+                episode: data
+            }]);
+        });
     });
 
      /**