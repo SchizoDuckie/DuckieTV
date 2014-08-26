139f9dbf4a408b993793bb80d728b52e04a75381
Js41637 ftw <js41637@gmail.com>
2014-08-26 10:04:44 +1000

    Fixed spelling errors
    How awkward

--- js/directives/serieDetails.js
+++ js/directives/serieDetails.js
@@ -53,9 +53,9 @@ angular.module('DuckieTV.directives.seriedetails', ['dialogs'])
             $scope.getAirDate = function(serie) {
                 return new Date(serie.firstaired).toString();
             };
-            
+
             /**
-             * Hide or display a show on the calender
+             * Hide or display a show on the calendar
              * @param object serie Plain Old Javascript Object
              * 
              * Because this method edits the db directly, dynamically updating
@@ -66,7 +66,7 @@ angular.module('DuckieTV.directives.seriedetails', ['dialogs'])
                 CRUD.FindOne('Serie', {
                     ID_Serie: show.ID_Serie
                 }).then(function(show2) {
-                    show2.set('displaycalender', 0);
+                    show2.set('displaycalendar', 0);
                     show2.Persist();
                     setTimeout(function(){window.location.reload(false)}, 650);
                 });
@@ -75,7 +75,7 @@ angular.module('DuckieTV.directives.seriedetails', ['dialogs'])
                 CRUD.FindOne('Serie', {
                     ID_Serie: show.ID_Serie
                 }).then(function(show2) {
-                    show2.set('displaycalender', 1);
+                    show2.set('displaycalendar', 1);
                     show2.Persist();
                     setTimeout(function(){window.location.reload(false)}, 650);
                 });
