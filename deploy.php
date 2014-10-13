c05e92cdbc9a805acb76a2f4bc3fba81754ece50
SchizoDuckie <schizoduckie@gmail.com>
2014-10-14 00:48:09 +0200

    swap out crappy php deployment script for sexy gulp.js buildscript. Check source for dependencies.

--- deploy.php
+++ deploy.php
@@ -1,53 +1,13 @@
 <title>DuckieTV - Deploying</title>
 <h1>DuckieTV Deployment Script</h1>
+<pre>
 
 <?php
 flush();
 ob_end_flush();
-$curDir = getcwd();
 
-if(!is_dir('../deploy')) mkdir('../deploy');
-if(!is_dir('../deploy/newtab'))	mkdir('../deploy/newtab');
-if(!is_dir('../deploy/browseraction')) mkdir('../deploy/browseraction');
-if(!is_dir('../deploy/opera')) mkdir('../deploy/opera');
+system('gulp deploy');
 
-
-file_put_contents('exclude.txt', "exclude.txt\r\n.git\r\nmanifest.json\r\nmanifest-app.json\r\ndeploy.php\r\ndeploytowebstoremacro.exe\r\ndeploy-both.mcr\r\ndeploy-newtab.mcr\r\ndeploy-browseraction.mcr\r\nchangelog.php\r\ndeploy-gh-pages.mcr\r\nduckietv.sublime-project\r\nduckietv.sublime-workspace\r\nmanifest-opera.js\r\n.nojekyll\r\nindex.html");
-echo "Copying to output dir.<pre style='height:150px; overflow-y:scroll'>";
-system('xcopy /EXCLUDE:exclude.txt /Y /E . ..\deploy\newtab');
-system('xcopy /EXCLUDE:exclude.txt /Y /E . ..\deploy\browseraction');
-system('xcopy /EXCLUDE:exclude.txt /Y /E . ..\deploy\opera');
-
-echo("</pre>");
-$newTabSettings = json_decode(file_get_contents('manifest.json'),true);
-$browserActionSettings = json_decode(file_get_contents('manifest-app.json'),true);
-$operaSettings = json_decode(file_get_contents('manifest-opera.json'),true);
-$version = trim(file_get_contents('VERSION'));
-system("git tag -am \"{$version}\" \"{$version}\"");
-//system("git push --tags");
-echo ("<h2>Current version: {$version}</h2>");
-$newTabSettings['version'] = $browserActionSettings['version'] = $operaSettings['version'] = $version;
-file_put_contents('../deploy/newtab/manifest.json', json_encode($newTabSettings,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-file_put_contents('../deploy/browseraction/manifest.json', json_encode($browserActionSettings,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-file_put_contents('../deploy/opera/manifest.json', json_encode($operaSettings,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-
-echo "Zipping new tab version<pre style='height:150px; overflow-y:scroll'>";
-chdir($curDir.'/../deploy/newtab');
-system("zip -R ../newtab-{$version}.zip *.*");
-echo "</pre>Zipping browser action version<pre style='height:150px; overflow-y:scroll'>";
-
-chdir($curDir.'/../deploy/browseraction');
-system("zip -R ../browseraction-{$version}.zip *.*");
-chdir($curDir.'/../deploy');
-system("rm -f browseraction-latest.zip");
-system("rm -f newtab-latest.zip");
-system("cp browseraction-{$version}.zip browseraction-latest.zip");
-system("cp newtab-{$version}.zip newtab-latest.zip");
-
-echo "</pre>";
-
-unlink("$curDir/exclude.txt");
-echo "<h1>Done</h1>";
 ?>
 <script type='text/javascript'>
 document.querySelector('title').innerText = 'DuckieTV - Deployment built.';
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * Gulp file (experimental) to concat all the scripts and minimize load time.
  * Usage:
  *
- * npm install gulp gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-notify gulp-rename gulp-replace --save-dev
+ * npm install gulp gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-notify gulp-rename gulp-replace gulp-json-editor js-beautify --save-dev
  * gulp
  */
 
@@ -15,7 +15,11 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     concatCss = require('gulp-concat-css'),
     replace = require('gulp-replace'),
-    notify = require('gulp-notify');
+    notify = require('gulp-notify'),
+    jsonedit = require("gulp-json-editor"),
+    zip = require('gulp-zip'),
+    fs = require('fs');
+
 
 // scripts are provided in order to prevent any problems with the load order dependencies
 var scripts = ['./js/controllers/*.js', './js/directives/*.js','./js/services/*.js', './js/app.js'];
@@ -25,13 +29,13 @@ var deps = ['./js/vendor/promise-3.2.0.js',
 './js/vendor/CRUD.js',
 './js/vendor/CRUD.SqliteAdapter.js',
 './js/CRUD.entities.js',
-"./js/vendor/angular.js",
+"./js/vendor/angular.min.js",
 "./js/vendor/angular-animate.js",
 "./js/vendor/angular-sanitize.min.js",
 "./js/vendor/angular-route.min.js",
 "./js/vendor/angular-xml.min.js",
-'./js/vendor/ui-bootstrap-tpls-0.11.2.min.js',
-'./js/vendor/tmhDynamicLocale.js',
+"./js/vendor/ui-bootstrap-tpls-0.10.0.min.js",
+"./js/vendor/tmhDynamicLocale.js",
 "./js/vendor/datePicker.js",
 "./js/vendor/dialogs.js",
 "./js/vendor/angular-translate.min.js",
@@ -47,28 +51,61 @@ var styles = [
     './css/flags.css'
 ];
 
+var background = [
+    "js/vendor/promise-3.2.0.js",
+    "js/vendor/CRUD.js",
+    "js/vendor/CRUD.SqliteAdapter.js",
+    "js/CRUD.entities.js",
+    "js/vendor/angular.min.js",
+    "js/vendor/angular-sanitize.min.js",
+    "js/directives/torrentDialog.js",
+    "js/services/SettingsService.js",
+    "js/services/StorageSyncService.js",
+    "js/services/SceneNameResolver.js",
+    "js/services/EventWatcherService.js",
+    "js/services/EventSchedulerService.js",
+    "js/services/EpisodeAiredService.js",
+    "js/services/FavoritesService.js",
+    "js/services/TraktTV.js",
+    "js/services/ThePirateBay.js",
+    "js/services/MirrorResolver.js",
+    "js/services/WatchlistCheckerService.js",
+    "js/services/WatchlistService.js",
+    "js/background.js"
+]
+
 gulp.task('default', function() {
     
     gulp.src(scripts)
-    .pipe(concat('app.js', {newLine: ';'}))
-    .pipe(gulp.dest('dist/'))
-    .pipe(notify({ message: 'Scripts packaged to dist/app.js' }));
+        .pipe(concat('app.js', {newLine: ';'}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'Scripts packaged to dist/app.js' }));
 
     /**
      * Package all the app's dependencies from the vendors foldr
      */
     gulp.src(deps)
-    .pipe(concat('deps.js', {newLine: ';'}))
-    .pipe(gulp.dest('dist/'))
-    .pipe(notify({ message: 'Deps packaged to dist/deps.js' }));
+        .pipe(concat('deps.js', {newLine: ';'}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'Deps packaged to dist/deps.js' }));
+
+    /**
+     * Package all the app's dependencies from the vendors foldr
+     */
+    gulp.src(background)
+        .pipe(concat('background.js', {newLine: ';'}))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'Background page packaged to dist/background.js' }));
+    gulp.src('launch.js')
+        .pipe(gulp.dest('dist/'));
 
     /**
      * replace tab.html's placeholders that indicate the deployment scripts 
      */
     gulp.src(['tab.html'])
-    .pipe(replace(/<!-- deploy:replace\=\'(.*)\' -->([\s\S]+?)[^\/deploy:]<!-- \/deploy:replace -->/g, '$1'))
-    .pipe(gulp.dest('dist/'))
-    .pipe(notify({ message: 'Tab template deployed' }));
+        .pipe(replace(/<!-- deploy:replace\=\'(.*)\' -->([\s\S]+?)[^\/deploy:]<!-- \/deploy:replace -->/g, '$1'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'Tab template deployed' }));
 
      gulp.src(styles)
         .pipe(concatCss("style.css"))
@@ -79,3 +116,70 @@ gulp.task('default', function() {
 
 });
  
+
+gulp.task('deploy', ['default'], function() {
+    // js-format formatting options used in manipulating manifest.json
+    var formatOptions = {
+        'indent_char': '\t',
+        'indent_size': 1,
+        'brace_style': 'end-expand'
+    }
+    // fetch duckietv version into variable.
+    var ver = String(fs.readFileSync('VERSION'));
+    
+    /**
+     * Modify package.json to remove the whole list of background scripts and replace it with dist version
+     */
+    var noLaunch = function(json) {
+        json.version = ver;
+        json.background.scripts = ['dist/background.js'];
+        return json;
+    }
+
+    /**
+     * Modify package.json to remove the whole list of background scripts and replace it with dist version and launch.js
+     */
+    var withLaunch = function(json) {
+        json.version = ver;
+        json.background.scripts = ['dist/background.js','dist/launch.js'];
+        return json;
+    }
+
+    // push all files to their dirs
+    gulp.src(['VERSION', '_locales/**','dist/**','fonts/**','img/**','templates/**'],{ "base" : "." })
+        .pipe(gulp.dest('../deploy/browseraction'))
+        .pipe(gulp.dest('../deploy/newtab'))
+        .pipe(gulp.dest('../deploy/opera'));
+    // move tab.html in place
+    gulp.src('dist/tab.html')
+        .pipe(gulp.dest('../deploy/browseraction'))
+        .pipe(gulp.dest('../deploy/newtab'))
+        .pipe(gulp.dest('../deploy/opera'));
+    // copy manifest.json variants into place and adapt it for deployment
+    gulp.src('manifest.json')
+        .pipe(jsonedit(noLaunch, formatOptions))
+        .pipe(gulp.dest('../deploy/newtab/'));
+    gulp.src('manifest-app.json')
+        .pipe(rename('manifest.json'))
+        .pipe(jsonedit(withLaunch, formatOptions))
+        .pipe(gulp.dest('../deploy/browseraction/'));
+    gulp.src('manifest-opera.json')
+        .pipe(rename('manifest.json'))
+        .pipe(jsonedit(withLaunch, formatOptions))
+        .pipe(gulp.dest('../deploy/opera/'));
+
+    gulp.src('../deploy/browseraction/**')
+        .pipe(zip('browseraction-'+ver+'.zip'))
+        .pipe(gulp.dest('../deploy'));
+
+    gulp.src('../deploy/newtab/**')
+        .pipe(zip('newtab-'+ver+'.zip'))
+        .pipe(gulp.dest('../deploy'));
+
+    return gulp.src('../deploy/opera/**')
+        .pipe(zip('opera-'+ver+'.zip'))
+        .pipe(gulp.dest('../deploy'))
+        .pipe(notify('deployment done to ../deploy/'));
+
+
+});
