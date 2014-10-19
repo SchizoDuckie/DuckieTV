2eed94919271a4964cf78299da16a9e6b4701e57
SchizoDuckie <schizoduckie@gmail.com>
2014-10-19 02:36:32 +0200

    Warn user onbeforepageunload when db queries haven't finished. Fixes #277

--- js/directives/queryMonitor.js
+++ js/directives/queryMonitor.js
@@ -9,14 +9,22 @@ angular.module('DuckieTV.directives.querymonitor',[])
         restrict: 'E',
         templateUrl: 'templates/querymonitor.html',
         link: function($scope, iElement) {
+
+            var unloadBreaker = function() {
+                return 'DuckieTV is still performing database operations. Do you really want to close this tab?';
+            } 
+
             $scope.queryStats = CRUD.stats;
             progress = 100;
 
             Object.observe(CRUD.stats, function() {
                 $scope.queryStats = CRUD.stats;
                 $scope.progress = Math.floor(($scope.queryStats.writesExecuted / $scope.queryStats.writesQueued )* 100);
+                window.onbeforeunload = ($scope.queryStats.writesExecuted < $scope.queryStats.writesQueued) ? unloadBreaker : null;
                 $scope.$digest();
             });
+
+            
         }
     }
 });
