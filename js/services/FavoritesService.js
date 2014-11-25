f3ae3cecbf3c8327b72f6e1cf3a24b177cced0d3
Js41637 ftw <js41637@gmail.com>
2014-11-25 22:25:29 +1100

    Update FavoritesService.js
    Spaces aren't needed, apparently?

--- js/services/FavoritesService.js
+++ js/services/FavoritesService.js
@@ -79,7 +79,7 @@ angular.module('DuckieTV.providers.favorites', [])
             });
         });
         CRUD.EntityManager.getAdapter().db.execute('delete from Episodes where ID_Serie = ? and TVDB_ID NOT IN (' + tvdbList.join(',') + ')', [ID]).then(function(result) {
-            console.log("Cleaned up ", result.rs.rowsAffected, " orphaned episodes");
+            console.log("Cleaned up", result.rs.rowsAffected, "orphaned episodes");
         });
         tvdbList = null;
     };
