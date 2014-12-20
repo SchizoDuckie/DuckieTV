8260db650489d372b0077c4938ca6c53834bd75c
garfield69 <garfield69@outlook.com>
2014-12-20 20:05:50 +1300

    [SerieCtrl] apply SceneNameResolver.getSearchStringForEpisode as per EpisodeCtrl

--- js/controllers/SerieCtrl.js
+++ js/controllers/SerieCtrl.js
@@ -110,7 +110,7 @@ angular.module('DuckieTV.controllers.serie', ['DuckieTV.directives.serieheader',
 
         $scope.getSearchString = function(serie, episode) {
             var serieName = SceneNameResolver.getSceneName(serie.TVDB_ID) || serie.name;
-            return serieName.replace(/\(([12][09][0-9]{2})\)/, '').replace(' and ', ' ') + ' ' + $scope.getEpisodeNumber(episode);
+            return serieName.replace(/\(([12][09][0-9]{2})\)/, '').replace(' and ', ' ') + ' ' + SceneNameResolver.getSearchStringForEpisode(serie, episode);
         };
 
         $scope.getEpisodeNumber = function(episode) {
