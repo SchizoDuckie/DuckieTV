3f56f950713c57afa9801c9337282b1c9807b70c
SchizoDuckie <schizoduckie@gmail.com>
2015-01-12 00:28:33 +0100

    Five whole tests. woohoo!
    (Now I can start finish the favoritesService.addToFavorites function)
    
    INFO [karma]: Karma v0.12.28 server started at http://localhost:9876/
    INFO [launcher]: Starting browser Chrome
    INFO [Chrome 39.0.2171 (Windows 8.1)]: Connected on socket HLIkIpHtK5SZhYKVTp4e with id 73489528
    
    Start:
      TrakTVv2
        It should be able to search for a serie
          √ should have executed a search for 'Doctor Who'
          √ Should be finding 10 items
          √ Should have Doctor Who as the second search result
      FavoritesService
        It should be initialized
          √ should have a favorites property
          √ should list all series
    
    Finished in 0.113 secs / 0.106 secs
    
    SUMMARY:
    √ 10 tests completed

--- tests/FavoritesService.test.js
+++ tests/FavoritesService.test.js
@@ -1,26 +1,101 @@
-describe('FavoritesService', function() {
+function fixture(url, cb) {
+    //var CryptoJS = require(['./js/vendor/sha1']);
+    url = './base/tests/fixtures/' + url.replace(/\W/g, '').toLowerCase() + '.json';
 
-    beforeEach(module('DuckieTV'));
-    beforeEach(inject(function(_FavoritesService_) {
-        FavoritesService = _FavoritesService_;
+    var x = new XMLHttpRequest();
+    x.open("GET", url, false);
+
+    x.send();
+
+    if (x.status === 200) {
+        var out = JSON.parse(x.responseText);
+        if (out.headers['Content-Type'].indexOf('json') > -1) {
+            return JSON.parse(out.content);
+        } else {
+            return out.content;
+        }
+    } else {
+        return null;
+    }
+}
+
+
+describe('TrakTVv2', function() {
+    var TraktTVv2, $httpBackend;
+
+    beforeEach(module('DuckieTV.providers.trakttvv2'));
+
+    beforeEach(inject(function(_$httpBackend_, _TraktTVv2_) {
+        TraktTVv2 = _TraktTVv2_;
+        $httpBackend = _$httpBackend_;
     }));
-    describe('It should be initialized', function() {
 
-        it('should have a favorites property', function(FavoritesService) {
-            var service = new FavoritesService();
-            expect(service).to.have.property('favorites', '[]');
+    beforeEach(inject(function($injector) {
+        // Set up the mock http service responses
+        $httpBackend = $injector.get('$httpBackend');
+
+        // todo: don't mind this.
+        $httpBackend.whenGET(/.*/).respond(function(method, url, data) {
+            var response = fixture(url);
+            return [response ? 200 : 404, response];
         });
+    }));
 
-        it('should list all series', function(FavoritesService) {
-            var service = new FavoritesService();
-            service.getSeries().then(function(series) {
-                console.info(series);
-                expect(series).to.be('array');
 
+    describe('It should be able to search for a serie', function() {
+
+        it("should have executed a search for 'Doctor Who'", function() {
+            TraktTVv2.search('Doctor Who').then(function(searchResults) {
+                expect(angular.isArray(searchResults)).toBe(true);
             });
+
+            $httpBackend.flush();
         });
+
+        it("Should be finding 10 items", function() {
+            TraktTVv2.search('Doctor Who').then(function(searchResults) {
+                expect(searchResults.length).toEqual(10);
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('Should have Doctor Who as the second search result', function() {
+            TraktTVv2.search('Doctor Who').then(function(searchResults) {
+                expect(searchResults[1].title).toMatch('Doctor Who');
+            });
+
+            $httpBackend.flush();
+        });
+
     });
 
+
+});
+
+describe('FavoritesService', function() {
+
+    var FavoritesService, $httpBackend;
+
+    beforeEach(module('DuckieTV.providers.favorites'));
+    beforeEach(inject(function(_FavoritesService_) {
+        FavoritesService = _FavoritesService_;
+
+    }));
+
+    describe('It should be initialized', function() {
+
+        it('should have a favorites property', function() {
+            expect(angular.isObject(FavoritesService.favorites)).toBe(true);
+        });
+
+        it('should list all series', function() {
+            FavoritesService.getSeries().then(function(result) {
+                expect(angular.isArray(result)).toBe(true);
+            });
+
+        });
+    });
 
 
 });
