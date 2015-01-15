60357c624b1e770d659ceb2730a40245d92c28fa
SchizoDuckie <schizoduckie@gmail.com>
2015-01-15 23:56:01 +0100

    Tweaked karma timeouts.

--- karma.conf.js
+++ karma.conf.js
@@ -73,11 +73,11 @@ module.exports = function(config) {
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
-
+        browserDisconnectTimeout: 60000,
+        browserNoActivityTimeout: 60000,
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
