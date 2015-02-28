4baa0714da266845d4905fe25629fe626a5f1dec
SchizoDuckie <schizoduckie@gmail.com>
2015-02-28 18:30:41 +0100

    fix sidepanel now auto-expanding on first click

--- js/directives/seriesList.js
+++ js/directives/seriesList.js
@@ -268,7 +268,7 @@ angular.module('DuckieTV.directives.serieslist', ['dialogs', 'DuckieTV.directive
         var serieslist = $scope.serieslist = this;
 
         this.width = SidePanelState.state.isExpanded ? document.body.clientWidth - 800 : SidePanelState.state.isShowing ? document.body.clientWidth - 400 : document.body.clientWidth;
-        this.activated = false; // Toggles when the favorites panel activated
+        this.activated = true; // Toggles when the favorites panel activated
 
         this.mode = SettingsService.get('series.displaymode'); // series display mode. Either 'banner' or 'poster', banner being wide mode, poster for portrait.
         this.isSmall = false; // Toggles the poster zoom
