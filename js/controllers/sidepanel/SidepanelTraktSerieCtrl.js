5b5f7ea6e7f17f8899a5716d6aac5ec0ac1f1831
Garfield69 <garfield69@outlook.com>
2015-03-05 06:37:47 +1300

    restore genre, status and dayOfWeek to sidepanel Trakt- serie-details

--- js/controllers/sidepanel/SidepanelTraktSerieCtrl.js
+++ js/controllers/sidepanel/SidepanelTraktSerieCtrl.js
@@ -1,4 +1,4 @@
-DuckieTV.controller('sidepanelTraktSerieCtrl', function($rootScope, $filter) {
+DuckieTV.controller('sidepanelTraktSerieCtrl', function($rootScope, $filter, $locale) {
 
     var sidepanel = this;
     this.serie = null;
@@ -8,98 +8,38 @@ DuckieTV.controller('sidepanelTraktSerieCtrl', function($rootScope, $filter) {
         $rootScope.$applyAsync();
     })
 
-
-    var daysOfWeekList = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-    ]; // used by translateDayOfWeek()
-
-    var genreList = [
-        'action',
-        'adventure',
-        'animation',
-        'children',
-        'comedy',
-        'crime',
-        'disaster',
-        'documentary',
-        'drama',
-        'eastern',
-        'family',
-        'fan-film',
-        'fantasy',
-        'film-noir',
-        'food',
-        'game-show',
-        'history',
-        'holiday',
-        'home-and-garden',
-        'horror',
-        'indie',
-        'mini-series',
-        'music',
-        'musical',
-        'mystery',
-        'news',
-        'none',
-        'reality',
-        'road',
-        'romance',
-        'science-fiction',
-        'short',
-        'soap',
-        'special-interest',
-        'sport',
-        'suspense',
-        'talk-show',
-        'thriller',
-        'travel',
-        'tv-movie',
-        'war',
-        'western'
-    ]; // used by translateGenre()
-    var rawTranslatedGenreList = $filter('translate')('SERIECTRLjs/genre/list');
-    var translatedGenreList = rawTranslatedGenreList.split(',');
-
-    var statusList = [
-        'canceled',
-        'ended',
-        'in production',
-        'returning series'
-    ]; // used by translateStatus()
-    var rawTranslatedStatusList = $filter('translate')('SERIECTRLjs/status/list');
-    var translatedStatusList = rawTranslatedStatusList.split(',');
-
     /*
-     * Takes a rating (8.12345) and coverts it percentage presentation (81)
+     * Takes a rating (8.12345) and converts it percentage presentation (81)
      */
     this.ratingPercentage = function(rating) {
         return Math.round(rating * 10);
     };
 
+    this.rawTranslatedGenreList = $filter('translate')('SERIECTRLjs/genre/list');
+    this.translatedGenreList = this.rawTranslatedGenreList.split(',');
+    this.genreList = 'action|adventure|animation|children|comedy|crime|disaster|documentary|drama|eastern|family|fan-film|fantasy|film-noir|food|game-show|history|holiday|home-and-garden|horror|indie|mini-series|music|musical|mystery|news|none|reality|road|romance|science-fiction|short|soap|special-interest|sport|suspense|talk-show|thriller|travel|tv-movie|war|western'.split('|'); // used by this.translateGenre()
+    /*
+     * Takes the English Genre (as fetched from TraktTV) and returns a translation
+     */
+    this.translateGenre = function(genre) {
+        return (this.genreList.indexOf(genre) != -1) ? this.translatedGenreList[this.genreList.indexOf(genre)] : genre;
+    };
+
+    this.daysOfWeekList = 'Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday'.split('|'); // used by this.translateDayOfWeek()
     /*
      * Takes the English day of the week (as fetched from TraktTV) and returns a translation
      */
     this.translateDayOfWeek = function(dayofweek) {
-        return $locale.DATETIME_FORMATS.DAY[daysOfWeekList.indexOf(dayofweek)];
+        return $locale.DATETIME_FORMATS.DAY[this.daysOfWeekList.indexOf(dayofweek)];
     };
 
-    /*
-     * Takes the English genre (as fetched from TraktTV) and returns a translation
-     */
-    this.translateGenre = function(genre) {
-        return (genreList.indexOf(genre) != -1) ? translatedGenreList[genreList.indexOf(genre)] : genre;
-    };
-
+    this.rawTranslatedStatusList = $filter('translate')('SERIECTRLjs/status/list');
+    this.translatedStatusList = this.rawTranslatedStatusList.split(',');
+    this.statusList = 'canceled|ended|in production|returning series'.split('|'); // used by this.translateStatus()
     /*
      * Takes the English status (as fetched from TraktTV) and returns a translation
      */
     this.translateStatus = function(status) {
-        return (statusList.indexOf(status) != -1) ? translatedStatusList[statusList.indexOf(status)] : status;
+        return (this.statusList.indexOf(status) != -1) ? this.translatedStatusList[this.statusList.indexOf(status)] : status;
     };
 });
