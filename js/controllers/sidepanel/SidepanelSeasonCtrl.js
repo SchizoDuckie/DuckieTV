0aa85de1db536b294f0fd62bf960d1b6b5495f7b
Garfield69 <garfield69@outlook.com>
2015-03-05 15:08:22 +1300

    restore the episode ratings chart.
    Moved from serie-details to episode-details.

--- js/controllers/sidepanel/SidepanelSeasonCtrl.js
+++ js/controllers/sidepanel/SidepanelSeasonCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Show one season
  */
-DuckieTV.controller('SidepanelSeasonCtrl', function(season, episodes, SceneNameResolver, EpisodeAiredService, $scope) {
+DuckieTV.controller('SidepanelSeasonCtrl', function(season, episodes, SceneNameResolver, EpisodeAiredService, $scope, $filter) {
     this.season = season;
     this.episodes = episodes;
 
@@ -29,5 +29,23 @@ DuckieTV.controller('SidepanelSeasonCtrl', function(season, episodes, SceneNameR
         return serieName.replace(/\(([12][09][0-9]{2})\)/, '').replace(' and ', ' ') + ' ' + SceneNameResolver.getSearchStringForEpisode(serie, episode);
     };
 
+    this.getEpisodeNumber = function(episode) {
+        var sn = episode.seasonnumber.toString(),
+            en = episode.episodenumber.toString(),
+            out = ['s', sn.length == 1 ? '0' + sn : sn, 'e', en.length == 1 ? '0' + en : en].join('');
+        return out;
+    };
+
+    this.points = [];
+    var data = $filter('orderBy')(this.episodes, this.getEpisodeNumber, false);
+    for (var i = 0; i < data.length; i++) {
+
+        this.points.push({
+            x: i,
+            y: data[i].rating,
+            label: this.getEpisodeNumber(data[i]) + ' : ' + data[i].rating,
+            season: parseInt(data[i].seasonnumber, 10)
+        });
+    };    
 
 })
