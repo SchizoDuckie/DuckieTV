d1f56342b7d5cd3b3c27da04a82dc9ca1df59ed2
garfield69 <garfield69@outlook.com>
2015-04-29 18:42:08 +1200

    round progress to one decimal place re #402

--- js/services/TorrentClients/Transmission.js
+++ js/services/TorrentClients/Transmission.js
@@ -246,7 +246,7 @@ DuckieTorrent
                         return this.name;
                     };
                     data.getProgress = function() {
-                        return Math.round(this.progress * 100);
+                        return Math.round((this.progress * 100) * 10) / 10.0; // round to one decimal place
                     }
                     data.start = function() {
                         DuckieTorrent.getClient().execute('torrent-start', this.id);
