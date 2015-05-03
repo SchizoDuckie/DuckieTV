e22fe0313537cf931308522bf51d42f88bea8e3a
Js41637 ftw <js41637@gmail.com>
2015-05-03 19:59:01 +1000

    Fix backup import not wiping before import
    Some bug cannot read .then of $scope.wipe, I dunno. I just changed it
    around a bit. Also put in an unused translation, not sure where it was
    originally meant to go but I just replaced the 'Loading please wait'
    with the 'You Confirmed No' if you declined to wipe database.

--- js/controllers/settings/BackupCtrl.js
+++ js/controllers/settings/BackupCtrl.js
@@ -23,9 +23,9 @@ DuckieTV.controller('BackupCtrl', ["$scope", "$dialogs", "$filter", "FileReader"
     function($scope, $dialogs, $filter, FileReader, TraktTVv2, SettingsService, FavoritesService, CalendarEvents, $q) {
 
         $scope.backupString = false;
-        $scope.series = [];
         $scope.wipeBeforeImport = false;
-
+        $scope.declined = false;
+        $scope.series = [];
 
         /**
          * Select all series from the database in the format described above, serialize them as a data uri string
@@ -77,58 +77,60 @@ DuckieTV.controller('BackupCtrl', ["$scope", "$dialogs", "$filter", "FileReader"
             return FavoritesService.isAdding(tvdb_id);
         };
 
+        
+        $scope.restore = function() {
+            console.log("Import backup!", $scope);
+            FavoritesService.flushAdding();
+            $scope.series = [];
+            if ($scope.wipeBeforeImport) {
+                $scope.wipeDatabase();
+            } else {
+                importBackup();
+            }
+        }
+
         /**
          * Read the backup file and feed it to the FavoritesService to resolve and add.
          * The Favoritesservice has a method to automagically import the watched episodes
          * (which is a bit hacky as it should be part of the import)
          */
-        $scope.restore = function() {
-            console.log("Import backup!", $scope);
-            FavoritesService.flushAdding();
-            $scope.series = [];
-            $q(function(resolve, reject) {
-                if ($scope.wipeBeforeImport) {
-                    $scope.wipe().then(resolve)
-                }
-                resolve();
-            }).then(function() {
-
-                FileReader.readAsText($scope.file, $scope)
-                    .then(function(result) {
-                        result = angular.fromJson(result);
-                        console.log("Backup read!", result);
-                        angular.forEach(result.settings, function(value, key) {
-                            if (key == 'utorrent.token') return; // skip utorrent auth token since it can be invalid.
-                            localStorage.setItem(key, value);
-                        });
-                        SettingsService.restore();
-                        angular.forEach(result.series, function(watched, TVDB_ID) {
-                            TraktTVv2.resolveTVDBID(TVDB_ID).then(function(searchResult) {
-                                return TraktTVv2.serie(searchResult.slug_id);
-                            }).then(function(serie) {
-                                FavoritesService.adding(TVDB_ID);
-                                $scope.series.push(serie);
-                                return FavoritesService.addFavorite(serie, watched);
-                            }).then(function() {
-                                FavoritesService.added(TVDB_ID);
-                            });
-                        });
-                    }, function(err) {
-                        console.error("ERROR!", err);
-                        FavoritesService.added(TVDB_ID);
-                        FavoritesService.addError(TVDB_ID,err);
+        var importBackup = function() {
+            FileReader.readAsText($scope.file, $scope)
+                .then(function(result) {
+                    result = angular.fromJson(result);
+                    console.log("Backup read!", result);
+                    angular.forEach(result.settings, function(value, key) {
+                        if (key == 'utorrent.token') return; // skip utorrent auth token since it can be invalid.
+                        localStorage.setItem(key, value);
                     });
+                    SettingsService.restore();
+                    angular.forEach(result.series, function(watched, TVDB_ID) {
+                        TraktTVv2.resolveTVDBID(TVDB_ID).then(function(searchResult) {
+                            return TraktTVv2.serie(searchResult.slug_id);
+                        }).then(function(serie) {
+                            FavoritesService.adding(TVDB_ID);
+                            $scope.series.push(serie);
+                            return FavoritesService.addFavorite(serie, watched);
+                        }).then(function() {
+                            FavoritesService.added(TVDB_ID);
+                        });
+                    });
+                }, function(err) {
+                    console.error("ERROR!", err);
+                    FavoritesService.added(TVDB_ID);
+                    FavoritesService.addError(TVDB_ID,err);
+                });
+        }
 
-            })
-
-        };
-
-        $scope.wipe = function() {
+        /**
+         * Wipes the database of all series, seasons and episodes and removes all settings
+         */
+       $scope.wipeDatabase = function() {
             var dlg = $dialogs.confirm($filter('translate')('BACKUPCTRLjs/wipe/hdr'),
                 $filter('translate')('BACKUPCTRLjs/wipe/desc') 
             );
             dlg.result.then(function(btn) {
-               var db = CRUD.EntityManager.getAdapter().db;
+                var db = CRUD.EntityManager.getAdapter().db;
                 for (var i in localStorage) {
                     if (i.indexOf('database.version') == 0) continue;
                     if (i.indexOf('utorrent.token') == 0) continue;
@@ -141,11 +143,14 @@ DuckieTV.controller('BackupCtrl', ["$scope", "$dialogs", "$filter", "FileReader"
 
                 return Promise.all(['Series', 'Seasons', 'Episodes'].map(function(table) {
                     return db.execute('DELETE from ' + table + ' where 1').then(function(result) {
+                        console.log("Database Deleted");
                         return true;
                     })
-                }))
+                })).then(function() {
+                    importBackup();
+                });
             }, function(btn) {
-                $scope.confirmed = $filter('translate')('BACKUPCTRLjs/wipe-cancelled/lbl');
+                $scope.declined = true;
             });
         };
     }
