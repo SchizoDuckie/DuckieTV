edb29139ddafe0f020cf21e3ade323b0b5074730
Garfield69 <garfield69@outlook.com>
2015-05-15 06:48:07 +1200

    this update to UpgradeNotificationService allows for both 1.0 and 1.1 to get change-log during this period of rapid updates which may cause some users to skip versions

--- js/services/UpgradeNotificationService.js
+++ js/services/UpgradeNotificationService.js
@@ -11,6 +11,20 @@ DuckieTV
 
         var dlgLinks = '<h2>Questions? Suggestions? Bugs? Kudo\'s?</h2>Find DuckieTV on <a href="http://reddit.com/r/DuckieTV" target="_blank">Reddit</a> or <a href="http://facebook.com/DuckieTV/" target="_blank">Facebook</a>.<br>If you find a bug, please report it on <a href="http://github.com/SchizoDuckie/DuckieTV/issues">Github</a></em>';
         var notifications = {
+            '1.0': ["<li>Completely revamped user interface (now with 100% more sexyness)",
+                "<li>Added support for Tixati, Transmission and qBittorrent torrent clients",
+                "<li>Added Strike and RarBG torrent search providers",
+                "<li>Added calendar grouping for netflix episode dumps",
+                "<li>Initial version of Subtitle search available from episodes panel",
+                "<li>Removed Chromecast integration. (Use getvideostream.com and the app!)",
+                "<li>Autodownloads now use the configured torrent provider",
+                "<li>Revamped the way torrent search engines are created and registered",
+                "<li>Shows can be marked as downloaded as well as watched, and downloaded episodes can be highlighted on the calendar",
+                "<li>Added Trakt.TV Trending category filters, caching for Trakt.TV trending list",
+                "<li>Fixes for DuckieTV standalone: now using frame-less window, open external links in default browser, window and unminimize from tray works in Ubuntu, added upgrade check and notification, and zoom control is now 1:1 with chrome browser",
+                "<li>Database performance improvement (including less frequent ratings updates)",
+                "<li>Added 'Watch on Netflix' button for Netflix shows - numerous other small changes and bugfixes to list "
+            ].join(''),
             '1.1': ["<li>Completely revamped user interface (now with 100% more sexyness)",
                 "<li>Added support for Tixati, Transmission and qBittorrent torrent clients",
                 "<li>Added Strike and RarBG torrent search providers",
