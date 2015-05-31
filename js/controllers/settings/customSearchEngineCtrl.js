0afc89e8de65c5303b3ce7132d91a636b2b84002
SchizoDuckie <schizoduckie@gmail.com>
2015-05-31 23:58:34 +0200

    able to add a custom search engine. (no persisting yet) re #18

--- js/controllers/settings/customSearchEngineCtrl.js
+++ js/controllers/settings/customSearchEngineCtrl.js
@@ -1,7 +1,10 @@
-DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearchEngines", "$q", "$http", "$injector",
-    function(SettingsService, TorrentSearchEngines, $q, $http, $injector) {
+DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearchEngines", "$q", "$http", "$injector","$scope",
+    function(SettingsService, TorrentSearchEngines, $q, $http, $injector, $scope) {
+
+        var self = this;
 
         this.model = {
+            testSearch: 'test',
             name: 'Torrentleech.org',
             mirror: 'http://torrentleech.org', // http://yoursite.com',
             searchEndpoint: '/torrents/browse/index/query/%s',
@@ -34,6 +37,8 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
             name: 'src',
         }];
 
+        self.status = 'Idle';
+
         this.fields = [{key: 'name', type: "input", templateOptions: { label: "Search Engine Name", type: "text" }},
             {key: 'mirror', type: "input", templateOptions: { label: "Base URL for site (exclude the final /)", type: "text" }},
             {key: 'searchEndpoint', type: "input", templateOptions: { label: "Search page url (use %s to inject search query)", type: "text" }},
@@ -50,31 +55,62 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
             {key: 'leecherProperty', type: "input", templateOptions: { label: "Leechers attribute", valueProp: 'name', options: attributeWhitelist }},
             {key: 'detailUrlSelector', type: "input", templateOptions: { label: "Detail URL Selector (page that opens in new tab and shows detail page for torrent)", type: "text" }},
             {key: 'detailUrlProperty', type: "input", templateOptions: { label: "Detail URL attribute", valueProp: 'name', options: attributeWhitelist }},
+            {key: 'testSearch', type: "input", templateOptions: { label: "Search query to use for testing", type: "text" }},
+
         ];
 
         this.test = function() {
+
+            self.status = 'creating test client';
+
             //console.log("Testing settings");
            var testClient =  new GenericTorrentSearchEngine({
-            mirror: this.model.mirror,  //'https://kat.cr',
-            noMagnet: true, 
-            endpoints: {
-                search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
-                details: '/torrent/%s'
-            },
-            selectors: {
-                resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
-                releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
-                torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
-                size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
-                seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
-                leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
-                detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
-            }
-        }, $q, $http, $injector);
-    
+                mirror: this.model.mirror,  //'https://kat.cr',
+                noMagnet: true, 
+                endpoints: {
+                    search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
+                    details: '/torrent/%s'
+                },
+                selectors: {
+                    resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
+                    releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
+                    torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
+                    size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
+                    seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
+                    leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
+                    detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
+                }
+            }, $q, $http, $injector);
+        
 
-        testClient.search('test').then(function(results) { console.log('Test search results!', results); });
+            self.status =  "Executing test search";
+            testClient.search(self.model.testSearch).then(function(results) { self.status = results.length > 0 ? 'Working!' : 'No results for search query :( '; $scope.$applyAsync; });
+
 
         };
+
+        this.add = function() {
+            TorrentSearchEngines.registerSearchEngine(this.model.name, 
+            new GenericTorrentSearchEngine({
+                mirror: this.model.mirror,  //'https://kat.cr',
+                noMagnet: true, 
+                endpoints: {
+                    search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
+                    details: '/torrent/%s'
+                },
+                selectors: {
+                    resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
+                    releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
+                    torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
+                    size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
+                    seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
+                    leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
+                    detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
+                }
+            }, $q, $http, $injector));
+
+            this.status = 'Done! check the torrent ialog!';
+        
+        }
     }
 ]);
