f9fd27effb98fca097c0fb0644396b0e151754f0
SchizoDuckie <schizoduckie@gmail.com>
2015-07-15 00:56:26 +0200

    DOMParser polyfill included.

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,35 @@
+/** DOMParser Polyfill */
+(function(DOMParser) {
+    "use strict";
+
+    var DOMParser_proto = DOMParser.prototype,
+        real_parseFromString = DOMParser_proto.parseFromString;
+
+    // Firefox/Opera/IE throw errors on unsupported types
+    try {
+        // WebKit returns null on unsupported types
+        if ((new DOMParser).parseFromString("", "text/html")) {
+            // text/html parsing is natively supported
+            return;
+        }
+    } catch (ex) {}
+
+    DOMParser_proto.parseFromString = function(markup, type) {
+        if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+            var
+                doc = document.implementation.createHTMLDocument("");
+            if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+                doc.documentElement.innerHTML = markup;
+            } else {
+                doc.body.innerHTML = markup;
+            }
+            return doc;
+        } else {
+            return real_parseFromString.apply(this, arguments);
+        }
+    };
+}(DOMParser));
+
 /**
  * Helper class for more fluent parsing of HTML results in a document parsed from string
  * Example:
