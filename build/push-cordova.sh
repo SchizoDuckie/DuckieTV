eb11f469b2c59c696d3f7a0dd02c744c5dd7872a
SchizoDuckie <schizoduckie@gmail.com>
2015-07-15 20:22:56 +0200

    Added DuckieTV online demo push to the cordova build script. This works around and fixes bug #497

--- build/push-cordova.sh
+++ build/push-cordova.sh
@@ -1,6 +1,8 @@
 cd /var/www/deploy/cordova
 git init
 git remote add origin git@github.com:SchizoDuckie/DuckieTV-Cordova.git
+git remote add duckietv git@github.com:DuckieTV/DuckieTV.git
 git add .
 git commit -m "Cordova deployment."
 git push --force origin master
+git push --force duckietv master:gh-pages
