310b3286515a287f7b68c7173fcc3f36f197da9d
SchizoDuckie <schizoduckie@gmail.com>
2015-07-18 21:14:00 +0200

    re-did subtitles and languages panel

--- js/controllers/settings/SubtitlesCtrl.js
+++ js/controllers/settings/SubtitlesCtrl.js
@@ -1,23 +1,31 @@
 DuckieTV.controller('SubtitlesCtrl', ['OpenSubtitles', 'SettingsService',
     function(OpenSubtitles, SettingsService) {
         var vm = this;
+        var allLanguages = OpenSubtitles.getLangages();
+        var reverseLanguages = {};
 
-        this.languages = OpenSubtitles.getLangages();
+        Object.keys(allLanguages).map(function(key) {
+            reverseLanguages[allLanguages[key]] = key;
+        });
+
+        this.languages = Object.keys(allLanguages).map(function(key) {
+            return allLanguages[key];
+        });
         this.codes = OpenSubtitles.getShortCodes();
 
         this.enabled = SettingsService.get('subtitles.languages');
 
         this.isEnabled = function(code) {
-            return this.enabled.indexOf(code) > -1;
+            return this.enabled.indexOf(reverseLanguages[code]) > -1;
         };
 
-        this.getShortCode = function(code) {
-            return this.codes[code];
+        this.getShortCode = function(lang) {
+            return this.codes[reverseLanguages[lang]];
         };
 
         this.getEnabledLanguages = function() {
             this.enabledLanguages = this.enabled.map(function(code) {
-                return vm.languages[code];
+                return allLanguages[code];
             }).join(', ');
             return this.enabledLanguages;
         };
@@ -29,11 +37,12 @@ DuckieTV.controller('SubtitlesCtrl', ['OpenSubtitles', 'SettingsService',
         };
 
         this.toggleSubtitle = function(language) {
-            //console.debug('togglesubtitle', language);
-            if (this.enabled.indexOf(language) === -1) {
-                this.enabled.push(language);
+            console.debug('togglesubtitle', language, reverseLanguages[language]);
+            var lang = reverseLanguages[language];
+            if (this.enabled.indexOf(lang) === -1) {
+                this.enabled.push(lang);
             } else {
-                this.enabled.splice(this.enabled.indexOf(language), 1);
+                this.enabled.splice(this.enabled.indexOf(lang), 1);
             }
             SettingsService.set('subtitles.languages', this.enabled);
             this.getEnabledLanguages();
