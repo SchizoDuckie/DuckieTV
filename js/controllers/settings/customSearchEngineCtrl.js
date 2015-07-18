f951d7295d40441692b13b50e70a8105caa35eb8
Js41637 ftw <js41637@gmail.com>
2015-07-19 01:53:16 +1000

    Initial Dialog support for customSearchEngines
    Made is so editing an engine now opens the dialog with the one you
    selected in a sexeh new dialog.
    
    Has the same functionality as before so you can edit it and then add it
    but only for the current session. So is mostly just a visual change.

--- js/controllers/settings/customSearchEngineCtrl.js
+++ js/controllers/settings/customSearchEngineCtrl.js
@@ -1,12 +1,14 @@
-DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearchEngines", "$q", "$http", "$injector", "$scope",
-    function(SettingsService, TorrentSearchEngines, $q, $http, $injector, $scope) {
+DuckieTV.controller("customSearchEngineCtrl", ["$scope", "$injector", "$http", "$q", "SettingsService", "TorrentSearchEngines", "dialogs",
+    function($scope, $injector, $http, $q, SettingsService, TorrentSearchEngines, dialogs) {
 
         var self = this;
-        self.status = 'Idle';
+        this.status = 'Idle';
         this.editMode = false;
         this.currentlySelected = null;
         this.model = null;
 
+        // List of current engines for testing. When actual saving functionality is added we need to load
+        // the engines here first and then send them over to the dialog along with the $index of what one was selected
         this.customEngines = {
             'Torrentleech': {
                 testSearch: 'test',
@@ -23,13 +25,13 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 sizeSelector: 'td:nth-child(5)',
                 sizeProperty: 'innerText',
                 seederSelector: 'td.seeders',
-                seederProperty: 'innerHTML',
+                seederProperty: 'innerText',
                 leecherSelector: 'td.leechers',
-                leecherProperty: 'innerHTML',
+                leecherProperty: 'innerText',
                 detailUrlSelector: 'td.name .title a',
                 detailUrlProperty: 'href'
             },
-            'Test 1': {
+            'Torrentleech2': {
                 testSearch: 'test',
                 name: 'Torrentleech2.org',
                 mirror: 'http://torrentleech.org', // http://yoursite.com',
@@ -44,13 +46,13 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 sizeSelector: 'td:nth-child(5)',
                 sizeProperty: 'innerText',
                 seederSelector: 'td.seeders',
-                seederProperty: 'innerHTML',
+                seederProperty: 'innerText',
                 leecherSelector: 'td.leechers',
-                leecherProperty: 'innerHTML',
+                leecherProperty: 'innerText',
                 detailUrlSelector: 'td.name .title a',
                 detailUrlProperty: 'href'
             },
-            'Test 2': {
+            'Torrentleech3': {
                 testSearch: 'test',
                 name: 'Torrentleech3.org',
                 mirror: 'http://torrentleech.org', // http://yoursite.com',
@@ -65,19 +67,102 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 sizeSelector: 'td:nth-child(5)',
                 sizeProperty: 'innerText',
                 seederSelector: 'td.seeders',
-                seederProperty: 'innerHTML',
+                seederProperty: 'innerText',
                 leecherSelector: 'td.leechers',
-                leecherProperty: 'innerHTML',
+                leecherProperty: 'innerText',
                 detailUrlSelector: 'td.name .title a',
                 detailUrlProperty: 'href'
             }
         };
 
+        
+
+        this.test = function(index) {
+            if(index && index != this.currentlySelected) {
+                this.editMode = false;
+                this.currentlySelected = null;
+                this.model = angular.copy(this.customEngines[Object.keys(this.customEngines)[index]]);
+            }
+
+            self.status = 'creating test client';
+
+            //console.log("Testing settings");
+            var testClient = new GenericTorrentSearchEngine({
+                mirror: this.model.mirror, //'https://kat.cr',
+                noMagnet: true,
+                endpoints: {
+                    search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
+                    details: '/torrent/%s'
+                },
+                selectors: {
+                    resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
+                    releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
+                    torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
+                    size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
+                    seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
+                    leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
+                    detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
+                }
+            }, $q, $http, $injector);
+
+            self.status = "Executing test search";
+            testClient.search(self.model.testSearch).then(function(results) {
+                self.status = results.length > 0 ? 'Working!' : 'No results for search query :( ';
+                $scope.$applyAsync();
+            });
+        };
+
+        this.addNew = function() {
+            
+        };
+
+        this.openDialog = function(index) {
+            // Opens dialog and sends list of current engines and the index of the one we are editing
+            dialogs.create('templates/customSearchEngineDialog.html', 'customSearchEngineDialogCtrl as cse', {
+                selected: index,
+                engines: self.customEngines
+            }, {
+                size: 'lg'
+            });
+        }
+    }
+])
+
+.controller('customSearchEngineDialogCtrl', ['$scope', "$injector", "$http", "$q", "data", "TorrentSearchEngines",
+    function($scope, $injector, $http, $q, data, TorrentSearchEngines) {
+
+        // Selects current engine from the index that is sent over, depending on how this is refactored
+        // We need to handle when adding a new engine where there will be no index... and nothing to copy.
+        this.model = angular.copy(data.engines[Object.keys(data.engines)[data.selected]]);
+
+        // Not sure how this handles adding new ones but we need to check against adding engines with same name
+        // to update it. Also TorrentSearchEngines currently doesn't support removing or enable/disable-ing engines
+        this.add = function() {
+            TorrentSearchEngines.registerSearchEngine(this.model.name, 
+                new GenericTorrentSearchEngine({
+                    mirror: this.model.mirror,
+                    noMagnet: true,
+                    endpoints: {
+                        search: this.model.searchEndpoint,
+                        details: '/torrent/%s'
+                    },
+                    selectors: {
+                        resultContainer: this.model.searchResultsContainer,
+                        releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty],
+                        torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty],
+                        size: [this.model.sizeSelector, this.model.sizeProperty],
+                        seeders: [this.model.seederSelector, this.model.seederProperty],
+                        leechers: [this.model.leecherSelector, this.model.leecherProperty],
+                        detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty]
+                    }
+                }, $q, $http, $injector));
+        }
+
         var attributeWhitelist = [{
             name: 'href',
-        }, {
+        }, /*{
             name: 'innerHTML',
-        }, {
+        }, */{
             name: 'innerText',
         }, {
             name: 'title',
@@ -115,6 +200,7 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'releaseNameSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: "Release name Selector (within base element)",
@@ -122,14 +208,16 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'releaseNameProperty',
+                className: 'cseProperty',
                 type: "select",
                 templateOptions: {
-                    label: "Release name attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
             }, {
                 key: 'torrentUrlSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: ".torrent URL selector (hyperlink to the torrent file)",
@@ -137,14 +225,16 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'torrentUrlProperty',
-                type: "input",
+                className: 'cseProperty',
+                type: "select",
                 templateOptions: {
-                    label: ".torrent URL attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
             }, {
                 key: 'sizeSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: "Size Selector (element that has the Torrent's size)",
@@ -152,14 +242,16 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'sizeProperty',
-                type: "input",
+                className: 'cseProperty',
+                type: "select",
                 templateOptions: {
-                    label: "Size attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
             }, {
                 key: 'seederSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: "Seeders Selector (element that has the 'seeders')",
@@ -167,14 +259,16 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'seederProperty',
-                type: "input",
+                className: 'cseProperty',
+                type: "select",
                 templateOptions: {
-                    label: "Seeders attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
             }, {
                 key: 'leecherSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: "Leechers Selector (element that has the 'leechers')",
@@ -182,14 +276,16 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'leecherProperty',
-                type: "input",
+                className: 'cseProperty',
+                type: "select",
                 templateOptions: {
-                    label: "Leechers attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
             }, {
                 key: 'detailUrlSelector',
+                className: 'cseSelector',
                 type: "input",
                 templateOptions: {
                     label: "Detail URL Selector (page that opens in new tab and shows detail page for torrent)",
@@ -197,9 +293,10 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             }, {
                 key: 'detailUrlProperty',
-                type: "input",
+                className: 'cseProperty',
+                type: "select",
                 templateOptions: {
-                    label: "Detail URL attribute",
+                    label: "Attribute",
                     valueProp: 'name',
                     options: attributeWhitelist
                 }
@@ -212,79 +309,5 @@ DuckieTV.controller("customSearchEngineCtrl", ["SettingsService", "TorrentSearch
                 }
             },
         ];
-
-        this.test = function(index) {
-            if(index && index != this.currentlySelected) {
-                this.editMode = false;
-                this.currentlySelected = null;
-                this.model = angular.copy(this.customEngines[Object.keys(this.customEngines)[index]]);
-            }
-
-            self.status = 'creating test client';
-
-            //console.log("Testing settings");
-            var testClient = new GenericTorrentSearchEngine({
-                mirror: this.model.mirror, //'https://kat.cr',
-                noMagnet: true,
-                endpoints: {
-                    search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
-                    details: '/torrent/%s'
-                },
-                selectors: {
-                    resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
-                    releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
-                    torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
-                    size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
-                    seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
-                    leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
-                    detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
-                }
-            }, $q, $http, $injector);
-
-            self.status = "Executing test search";
-            testClient.search(self.model.testSearch).then(function(results) {
-                self.status = results.length > 0 ? 'Working!' : 'No results for search query :( ';
-                $scope.$applyAsync();
-            });
-        };
-
-        this.selectEngine = function(index) {
-            if (this.currentlySelected == index) {
-                this.editMode = false;
-                this.currentlySelected = null;
-                return;
-            }
-            this.currentlySelected = index;
-            this.model = angular.copy(this.customEngines[Object.keys(this.customEngines)[index]]);
-            this.editMode = true;
-        };
-
-        this.addNew = function() {
-            
-        };
-
-        this.add = function() {
-            TorrentSearchEngines.registerSearchEngine(this.model.name,
-                new GenericTorrentSearchEngine({
-                    mirror: this.model.mirror, //'https://kat.cr',
-                    noMagnet: true,
-                    endpoints: {
-                        search: this.model.searchEndpoint, //'/usearch/%s/?field=seeders&sorder=desc',
-                        details: '/torrent/%s'
-                    },
-                    selectors: {
-                        resultContainer: this.model.searchResultsContainer, //'table.data tr[id^=torrent]',
-                        releasename: [this.model.releaseNameSelector, this.model.releaseNameProperty], //, ['div.torrentname a.cellMainLink', 'innerText'],
-                        torrentUrl: [this.model.torrentUrlSelector, this.model.torrentUrlProperty], //['a[title="Torrent magnet link"]', 'href'],
-                        size: [this.model.sizeSelector, this.model.sizeProperty], // ['td:nth-child(2)', 'innerText'],
-                        seeders: [this.model.seederSelector, this.model.seederProperty], // ['td:nth-child(5)', 'innerHTML'],
-                        leechers: [this.model.leecherSelector, this.model.leecherProperty], // ['td:nth-child(6)', 'innerHTML'],
-                        detailUrl: [this.model.detailUrlSelector, this.model.detailUrlProperty] // ['div.torrentname a.cellMainLink', 'href']
-                    }
-                }, $q, $http, $injector));
-
-            this.status = 'Done! check the torrent dialog!';
-
-        }
     }
-]);
+])
