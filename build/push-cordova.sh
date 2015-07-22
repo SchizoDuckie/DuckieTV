8a49ec7a22373623450b2a6b73ed8483ad7a2003
SchizoDuckie <schizoduckie@gmail.com>
2015-07-22 23:02:01 +0200

    fixed auto-push (doh)

--- build/push-cordova.sh
+++ build/push-cordova.sh
@@ -1,8 +1,9 @@
 cd /var/www/deploy/cordova
+rm ./.git -rf
 git init
 git remote add origin git@github.com:SchizoDuckie/DuckieTV-Cordova.git
 git remote add duckietv git@github.com:DuckieTV/DuckieTV.git
-git add . --quiet
+git add .
 git commit -m "Cordova deployment."
 echo "pushing to SchizoDuckie/DuckieTV-Cordova:master"
 git push --force origin master
