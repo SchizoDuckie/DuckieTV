2861ee4a0fb32cc58d626c09fd4d3881aaff05c5
SchizoDuckie <schizoduckie@gmail.com>
2015-08-23 00:40:33 +0200

    w.i.p series torrent settings re #484

--- js/controllers/settings/SerieTorrentSettingsCtrl.js
+++ js/controllers/settings/SerieTorrentSettingsCtrl.js
@@ -1,7 +1,7 @@
 DuckieTV.controller('serieTorrentSettingsCtrl', function($scope, $modalInstance, data) {
 
     $scope.model = data.serie;
-
+    $scope.model.autoDownload = $scope.model.autoDownload == 1;
     $scope.fields = [{
         key: "customSearchString",
         type: "input",
@@ -14,10 +14,17 @@ DuckieTV.controller('serieTorrentSettingsCtrl', function($scope, $modalInstance,
         key: "autoDownload",
         type: "checkbox",
         templateOptions: {
-            label: "Auto-Download this show"
+            label: "Auto-Download this show",
+            class: 'form-control'
         },
 
     }];
+
+    $scope.save = function() {
+        $scope.model.autoDownload = $scope.model.autoDownload ? 1 : 0;
+        $scope.model.Persist();
+        $modalInstance.dismiss();
+    };
 
     $scope.cancel = function() {
         $modalInstance.dismiss('Canceled');
