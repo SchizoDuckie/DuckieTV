f61d725774cd8e4abef3fdd1cad38a0e93b3a746
SchizoDuckie <schizoduckie@gmail.com>
2015-08-26 21:55:22 +0200

    fastsearch w.i.p. added searches and fast handling of typing while dialog isn't done rendering yet re #537

--- js/directives/fastSearch.js
+++ js/directives/fastSearch.js
@@ -1,31 +1,47 @@
-DuckieTV.directive('fastSearch', ["$window", "dialogs",
+DuckieTV.directive('fastSearch', ["$window", "dialogs", "$rootScope",
 
-    function($window, dialogs) {
+    function($window, dialogs, $rootScope) {
+        var self = this;
+
+        this.query = '';
         console.warn("fastsearch initializing");
         var isShowing = false;
 
-        function createDialog(initialQuery) {
+        this.focusInput = function() {
+            var i = document.querySelector(".fastsearch input");
+            if (i) {
+                i.value = self.query;
+                i.focus();
+                var e = document.createEvent("HTMLEvents");
+                e.initEvent('onchange', true, true);
+                i.dispatchEvent(e);
+            } else {
+                setTimeout(focusInput, 50);
+            }
+        };
+
+        this.createDialog = function() {
             isShowing = true;
             var d = dialogs.create('templates/fastSearch.html', 'fastSearchCtrl', {
-                key: initialQuery
+                key: self.query
             }, {
                 size: 'xs'
             });
 
-            setTimeout(function() {
-                document.querySelector(".fastsearch input").focus();
-            }, 0);
+            setTimeout(focusInput, 50);
 
             d.result.then(function() {
                 //console.debug('Success');
                 d = undefined;
                 isShowing = false;
+                self.query = '';
             }, function() {
                 //console.debug('Cancelled');
                 d = undefined;
                 isShowing = false;
+                self.query = '';
             });
-        }
+        };
 
         return {
             restrict: 'E',
@@ -34,9 +50,9 @@ DuckieTV.directive('fastSearch', ["$window", "dialogs",
                 this.keys = '';
                 console.warn("fastsearch initialized");
                 $window.addEventListener('keypress', function(e) {
-                    console.log("key pressed: ", e);
+                    self.query += String.fromCharCode(e.charCode);
                     if (!isShowing && e.target.tagName.toLowerCase() != 'input') {
-                        createDialog(String.fromCharCode(e.charCode));
+                        self.createDialog();
                     }
                 });
             }
@@ -44,8 +60,8 @@ DuckieTV.directive('fastSearch', ["$window", "dialogs",
     }
 ])
 
-.controller('fastSearchCtrl', ["$scope", "data", "FavoritesService", "TraktTVv2",
-    function($scope, data, FavoritesService, TraktTVv2) {
+.controller('fastSearchCtrl', ["$scope", "data", "FavoritesService", "TraktTVv2", "$rootScope",
+    function($scope, data, FavoritesService, TraktTVv2, $rootScope) {
 
         $scope.hasFocus = true;
         $scope.model = {
@@ -54,6 +70,7 @@ DuckieTV.directive('fastSearch', ["$window", "dialogs",
 
         $scope.searchResults = {
             series: [],
+            traktSeries: [],
             episodes: [],
             actors: []
         };
@@ -63,7 +80,7 @@ DuckieTV.directive('fastSearch', ["$window", "dialogs",
             type: "input",
             templateOptions: {
                 label: "Search for anything",
-                placeholder: "series in your favorites, new series to add, episodes, torrents, actors",
+                placeholder: "series in your favorites, new series to add, episodes, torrents",
                 type: "text",
                 onChange: function(e) {
                     $scope.search(e);
@@ -71,13 +88,30 @@ DuckieTV.directive('fastSearch', ["$window", "dialogs",
             }
         }];
 
+
+        $scope.getSerie = function(episode) {
+            return FavoritesService.getByID_Serie(episode.ID_Serie);
+        };
+
         $scope.search = function(value) {
             $scope.searchResults.series = FavoritesService.favorites.filter(function(serie) {
                 return serie.name.toLowerCase().indexOf(value.toLowerCase()) > -1;
             });
 
+            CRUD.Find("Episode", Array("episodename like '%" + value + "%'")).then(function(result) {
+                $scope.searchResults.episodes = result;
+                $rootScope.$applyAsync();
+            });
+
+            TraktTVv2.search(value).then(function(results) {
+                $scope.searchResults.traktSeries = results;
+                $rootScope.$applyAsync();
+            });
 
         };
+
+        $scope.search(data.key);
+
 
 
     }
