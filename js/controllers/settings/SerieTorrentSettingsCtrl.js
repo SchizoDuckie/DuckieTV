318f1ad2a088dffc92f08f0712d7f6e67bac5c93
SchizoDuckie <schizoduckie@gmail.com>
2015-08-30 11:42:06 +0200

    Moved serietorrentsettings formly form to json

--- js/controllers/settings/SerieTorrentSettingsCtrl.js
+++ js/controllers/settings/SerieTorrentSettingsCtrl.js
@@ -1,29 +1,11 @@
-DuckieTV.controller('serieTorrentSettingsCtrl', function($scope, $filter, $modalInstance, FavoritesService, data) {
+DuckieTV.controller('serieTorrentSettingsCtrl', function($scope, $filter, $modalInstance, FavoritesService, FormlyLoader, data) {
     console.info("Reinitcontroller!");
     $scope.model = FavoritesService.getById(data.serie.TVDB_ID); // refresh the model because it's cached somehow by the $modalInstance. (serialisation probably)
     $scope.model.autoDownload = $scope.model.autoDownload == 1;
-    $scope.fields = [{
-        key: "customSearchString",
-        type: "input",
-        templateOptions: {
-            label: $filter('translate')('SERIETORRENTSETTINGSCTRLjs/custom-search/lbl'),
-            placeholder: $filter('translate')('SERIETORRENTSETTINGSCTRLjs/custom-search/placeholder'),
-            type: "text",
-        }
-    }, {
-        className: 'row',
-        fieldGroup: [{
-            key: 'autoDownload',
-            className: 'inline-checkbox',
-            type: "input",
-            templateOptions: {
-                label: $filter('translate')('SERIETORRENTSETTINGSCTRLjs/auto-download/lbl'),
-                type: "checkbox"
-            }
-        }]
-    }];
 
-
+    FormlyLoader.load('SerieTorrentSettings').then(function(form) {
+        $scope.fields = form;
+    });
 
     $scope.save = function() {
         $scope.model.autoDownload = $scope.model.autoDownload ? 1 : 0;
