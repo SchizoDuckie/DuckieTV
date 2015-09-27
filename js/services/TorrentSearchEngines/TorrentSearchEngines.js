366b75bdea46a8d37fb4f57485536b6c3a03f8ae
garfield69 <garfield69@outlook.com>
2015-09-27 20:47:57 +1300

    fix #559
    resolve TypeError: undefined is not a function at Object.service.findEpisode (TorrentSearchEngines.js:75)

--- js/services/TorrentSearchEngines/TorrentSearchEngines.js
+++ js/services/TorrentSearchEngines/TorrentSearchEngines.js
@@ -72,13 +72,12 @@ DuckieTV.factory('TorrentSearchEngines', ["DuckieTorrent", "$rootScope", "dialog
             },
 
             findEpisode: function(serie, episode) {
-                return SceneNameResolver.getSearchStringForEpisode(serie, episode).then(function(seasonepisode) {
-                    return dialogs.create('templates/torrentDialog.html', 'torrentDialogCtrl', {
-                        query: SceneNameResolver.getSceneName(serie.TVDB_ID, serie.name) + ' ' + seasonepisode,
-                        TVDB_ID: episode.TVDB_ID
-                    }, {
-                        size: 'lg'
-                    });
+                var seasonepisode = SceneNameResolver.getSearchStringForEpisode(serie, episode);
+                return dialogs.create('templates/torrentDialog.html', 'torrentDialogCtrl', {
+                    query: SceneNameResolver.getSceneName(serie.TVDB_ID, serie.name) + ' ' + seasonepisode,
+                    TVDB_ID: episode.TVDB_ID
+                }, {
+                    size: 'lg'
                 });
 
             },
