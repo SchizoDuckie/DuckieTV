08a02fc2da82a4dbde50256b4e5598cd08069210
tvduckie@gmail.com <schizoduckie+duckietv@gmail.com>
2015-09-27 17:50:44 -0400

    Fixed push-xem script.

--- build/push-xem.sh
+++ build/push-xem.sh
@@ -1,9 +1,9 @@
 cd /var/www/deploy/xem
 git init
-git remote add duckietv git@github.com:DuckieTV/xem-cache.git
+git remote add origin git@github.com:DuckieTV/xem-cache.git
 git checkout gh-pages
 git pull origin gh-pages
 git add .
 git commit -m "XEM Cache update"
 echo "pushing to DuckieTV/xem-cache:gh-pages"
-git push duckietv master:gh-pages -f
+git push origin master:gh-pages -f
