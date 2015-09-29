c2216677a2fc245a9ad23e98a5de5bf84b40e9af
garfield69 <garfield69@outlook.com>
2015-09-29 18:50:19 +1300

    restore torrentSettings code removed by b1bcec51548ac168282115e7012e5f5c3708f4a8
    it's used by templates\sidepanel\serie-overview.html

--- js/controllers/sidepanel/SidepanelSerieCtrl.js
+++ js/controllers/sidepanel/SidepanelSerieCtrl.js
@@ -83,6 +83,24 @@ DuckieTV.controller('SidepanelSerieCtrl', function(dialogs, $rootScope, $scope, 
         }
     };
 
+    this.torrentSettings = function() {
+        var d = dialogs.create('templates/settings/serieTorrentSettings.html', 'serieTorrentSettingsCtrl', {
+            serie: self.serie
+        }, {
+            bindToController: true,
+            size: 'xs'
+        });
+
+        d.result.then(function() {
+            //console.debug('Success');
+            d = undefined;
+        }, function() {
+            //console.debug('Cancelled');
+            d = undefined;
+
+        });
+    };
+
     this.toggleSerieDisplay = function() {
         this.serie.displaycalendar = this.serie.displaycalendar == '1' ? '0' : '1';
         this.serie.Persist();
