1e96b61da4d56d1b4354bfe0fc8a0f7af8cdea93
garfield69 <garfield69@outlook.com>
2015-09-29 15:12:58 +1300

    replace reduction
    @js41637 aw ok then, how about this then? ;-)

--- js/services/SceneNameResolver.js
+++ js/services/SceneNameResolver.js
@@ -25,7 +25,7 @@ DuckieTV.factory('SceneNameResolver', ["$q", "$http", "SceneXemResolver",
              */
             getSceneName: function(tvdbID, name) {
                 tvdbID = parseInt(tvdbID);
-                return (tvdbID in exceptions) ? exceptions[tvdbID].replace(/\(([12][09][0-9]{2})\)/, '').replace('!', '').replace('(', '').replace(')', '').replace(' and ', ' ').replace(/\'/g, '') : name.replace(/\(([12][09][0-9]{2})\)/, '').replace('!', '').replace('(', '').replace(')', '').replace(' and ', ' ').replace(/\'/g, '');
+                return (tvdbID in exceptions) ? exceptions[tvdbID].replace(/\(([12][09][0-9]{2})\)/, '').replace(/\!|\(|\)|\'|\:/g, '').replace(' and ', ' ') : name.replace(/\(([12][09][0-9]{2})\)/, '').replace('!', '').replace(/\!|\(|\)|\'|\:/g, '').replace(' and ', ' ');
             },
 
             getSearchStringForEpisode: function(serie, episode) {
