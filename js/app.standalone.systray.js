5ab2f4488d44cfae49da6fcb82b93adbc44a5f74
garfield69 <garfield69@outlook.com>
2015-10-03 11:32:29 +1300

    prevent "Uncaught TypeError: Cannot read property 'remove' of undefined"
    when minimizing to taskbar.

--- js/app.standalone.systray.js
+++ js/app.standalone.systray.js
@@ -16,31 +16,33 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
             win.hide();
 
             // Show tray
-	    var menu =  new gui.Menu()
+            var menu =  new gui.Menu();
             tray = new gui.Tray({
                 title: navigator.userAgent,
                 icon: 'img/logo/icon64.png',
                 menu: menu
             });
-	    var trayClick = function() {
+            var trayClick = function() {
                 this.remove();
                 win.show();
                 win.emit('restore');
             }.bind(tray);
 
-	    if(navigator.platform.indexOf('Mac') > -1) {
-		var clickie = new gui.MenuItem({ label: 'Show DuckieTV' });
-            	clickie.on('click', trayClick);
-		menu.append(clickie);
-	    }
+          if(navigator.platform.indexOf('Mac') > -1) {
+            var clickie = new gui.MenuItem({ label: 'Show DuckieTV' });
+                clickie.on('click', trayClick);
+            menu.append(clickie);
+            };
 
             tray.tooltip = navigator.userAgent;
             tray.on('click', trayClick);
         }
     });
 
+    // get the restore event
     win.on('restore', function() {
-        console.log('removing tray.');
-        tray.remove();
+        if (window.localStorage.getItem('standalone.minimizeSystray') === 'Y') {
+            tray.remove();
+        }
     });
 }
