0f261cf988f99b03c2c846e86984ae30d751bb2b
Js41637 ftw <js41637@gmail.com>
2015-10-08 13:53:31 +1100

    Add exit menu option re: #569
    Added a prototype code for checking if database operations are ongoing.
    see comments for details.

--- js/app.standalone.systray.js
+++ js/app.standalone.systray.js
@@ -15,24 +15,37 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
             // Hide window
             win.hide();
 
-            // Show tray
             var menu =  new gui.Menu();
+
+            // Show tray
             tray = new gui.Tray({
                 title: navigator.userAgent,
                 icon: 'img/logo/icon64.png',
                 menu: menu
             });
+
             var trayClick = function() {
                 this.remove();
                 win.show();
                 win.emit('restore');
             }.bind(tray);
 
-          if(navigator.platform.indexOf('Mac') > -1) {
-            var clickie = new gui.MenuItem({ label: 'Show DuckieTV' });
-                clickie.on('click', trayClick);
-            menu.append(clickie);
-            };
+            var exitClick = function() {
+                this.remove();
+                win.close(true);
+            }.bind(tray);
+
+            // If on a mac add a show DTV Menu Button
+            if (navigator.platform.indexOf('Mac') > -1) {
+                var clickie = new gui.MenuItem({ label: 'Show DuckieTV' });
+                    clickie.on('click', trayClick);
+                menu.append(clickie);
+            }
+
+            // Add an exit button
+            var exitbtn = new gui.MenuItem({ label: 'Exit' });
+                exitbtn.on('click', exitClick);
+            menu.append(exitbtn);
 
             tray.tooltip = navigator.userAgent;
             tray.on('click', trayClick);
@@ -45,4 +58,33 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
             tray.remove();
         }
     });
+
+    // Only fires if force close is false
+    /* Prototype
+    win.on('close', function() {
+
+        win.showDevTools();
+
+        var queryStats = CRUD.stats;
+
+        /**
+         * When closing DuckieTV we don't currently check if there is any ongoing database operations
+         * It is possible to check as CRUD is global and we can continue to run the db updates in background
+         * until finished and then properly close the app.
+         * One issue however is that CRUDs 'writesQueued' isn't the correct number, more db operations can
+         * be added after it finishes which leaves like 1ms where 'Can close safely' function will fire incorrectly.
+         *//*
+        if (queryStats.writesExecuted < queryStats.writesQueued) {
+            Object.observe(CRUD.stats, function() {
+                queryStats = CRUD.stats;
+                if (queryStats.writesExecuted < queryStats.writesQueued) {
+                    console.log("Database operations still ongoing!");
+                } else {
+                    console.log("Can close safely, win.close(true) in console to close");
+                }
+            });
+        } else {
+            console.log("We can close safely, win.close(true) in console to close");
+        }
+    }); */
 }
