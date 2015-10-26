e19650c9f99a833b2973d4411ecdbf721c7e2570
SchizoDuckie <schizoduckie@gmail.com>
2015-10-26 20:18:42 +0100

    Fixed CMD+Q shortcut for exit

--- js/app.standalone.systray.js
+++ js/app.standalone.systray.js
@@ -16,7 +16,7 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
             win.hide();
 
             // create empty menu
-            var menu =  new gui.Menu();
+            var menu = new gui.Menu();
 
             // Show tray
             tray = new gui.Tray({
@@ -45,55 +45,72 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
                 win.emit('standalone.calendar');
                 trayClick();
             };
-            
+
             // handle favorites menu click
             var favoritesClick = function() {
                 win.emit('standalone.favorites');
                 trayClick();
             };
-            
+
             // handle settings menu click
             var settingsClick = function() {
                 win.emit('standalone.settings');
                 trayClick();
             };
-            
+
             // handle about menu click
             var aboutClick = function() {
                 win.emit('standalone.about');
                 trayClick();
             };
-            
+
             // add show menu
-            var menuShow = new gui.MenuItem({ label: 'Show DuckieTV' });
+            var menuShow = new gui.MenuItem({
+                label: 'Show DuckieTV'
+            });
             menuShow.on('click', trayClick);
             menu.append(menuShow);
 
             // add calendar menu
-            var menuCalendar = new gui.MenuItem({ label: 'Show Calendar' });
+            var menuCalendar = new gui.MenuItem({
+                label: 'Show Calendar'
+            });
             menuCalendar.on('click', calendarClick);
             menu.append(menuCalendar);
 
             // add favorites menu
-            var menuFavorites = new gui.MenuItem({ label: 'Show Favorites' });
+            var menuFavorites = new gui.MenuItem({
+                label: 'Show Favorites'
+            });
             menuFavorites.on('click', favoritesClick);
             menu.append(menuFavorites);
 
             // add settings menu
-            var menuSettings = new gui.MenuItem({ label: 'Show Settings' });
+            var menuSettings = new gui.MenuItem({
+                label: 'Show Settings'
+            });
             menuSettings.on('click', settingsClick);
             menu.append(menuSettings);
 
             // add about menu
-            var menuAbout = new gui.MenuItem({ label: 'Show About' });
+            var menuAbout = new gui.MenuItem({
+                label: 'Show About'
+            });
             menuAbout.on('click', aboutClick);
             menu.append(menuAbout);
 
             // add a separator to menu
-            menu.append(new gui.MenuItem({ type: 'separator' }));
+            menu.append(new gui.MenuItem({
+                type: 'separator'
+            }));
 
             // Add an exit menu
-            var menuExit = new gui.MenuItem({ label: 'Exit' });
+            var menuExit = new gui.MenuItem({
+                label: 'Exit',
+                modifiers: 'cmd-Q',
+                key: 'q'
+            });
+
             menuExit.on('click', exitClick);
             menu.append(menuExit);
 
@@ -121,7 +138,8 @@ if (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1) {
          * until finished and then properly close the app.
          * One issue however is that CRUDs 'writesQueued' isn't the correct number, more db operations can
          * be added after it finishes which leaves like 1ms where 'Can close safely' function will fire incorrectly.
-         *//*
+         */
+    /*
         if (queryStats.writesExecuted < queryStats.writesQueued) {
             Object.observe(CRUD.stats, function() {
                 queryStats = CRUD.stats;
