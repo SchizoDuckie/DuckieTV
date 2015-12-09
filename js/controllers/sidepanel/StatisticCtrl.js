1c335adfe7c9ad36fdf2a68254d2acf0749bed17
João Silva <joaosilva@revtut.net>
2015-12-09 10:59:19 +0000

    Updated statistic ctrl to have its own functions

--- js/controllers/sidepanel/StatisticCtrl.js
+++ js/controllers/sidepanel/StatisticCtrl.js
@@ -12,30 +12,29 @@
 DuckieTV.controller('StatisticCtrl', ["FavoritesService",
 
     function(FavoritesService) {
-        $scope.favorites = FavoritesService.favorites;
-    };
 
-    // Started working here
-    var watchTime = function() {
-        console.log("Im being called");
-        CRUD.executeQuery('SELECT Sum(runtime) AS watchTime FROM (SELECT runtime FROM Episodes ' +
-                        'INNER JOIN Series ON Series.ID_Serie = Episodes.ID_Serie WHERE Episodes.watched = 1)').then(function(result) {
-            return parseInt(result.next().get('watchTime'));
-        });
-    };
+        // Started working here
+        var watchTime = function() {
+            console.log("Im being called");
+            CRUD.executeQuery('SELECT Sum(runtime) AS watchTime FROM (SELECT runtime FROM Episodes ' +
+                            'INNER JOIN Series ON Series.ID_Serie = Episodes.ID_Serie WHERE Episodes.watched = 1)').then(function(result) {
+                return parseInt(result.next().get('watchTime'));
+            });
+        };
 
-    var watchSerie = function(){
-        //index 0 series watched index 1 series not watched
-        var watchedSerie = [];
-        CRUD.executeQuery('SELECT COUNT(Series.watched) AS watchedSerie FROM Series'+ 
-                            ' WHERE Series.watched = 1').then(function(result){
+        var watchSerie = function(){
+            //index 0 series watched index 1 series not watched
+            var watchedSerie = [];
+            CRUD.executeQuery('SELECT COUNT(Series.watched) AS watchedSerie FROM Series'+ 
+                                ' WHERE Series.watched = 1').then(function(result){
 
-            watchedSerie.push(parseInt(result.next().get('watchedSerie')));
-        });
-        CRUD.executeQuery('SELECT COUNT(Series.watched) AS nonWatchedSerie FROM Series '+
-                        ' WHERE Series.watched = 0').then(function(result){
-            watchedSerie.push(parseInt(result.next().get('nonWatchedSerie')));
-            return watchedSerie;
-        });
-    };
+                watchedSerie.push(parseInt(result.next().get('watchedSerie')));
+            });
+            CRUD.executeQuery('SELECT COUNT(Series.watched) AS nonWatchedSerie FROM Series '+
+                            ' WHERE Series.watched = 0').then(function(result){
+                watchedSerie.push(parseInt(result.next().get('nonWatchedSerie')));
+                return watchedSerie;
+            });
+        };
+    }
 ]);
