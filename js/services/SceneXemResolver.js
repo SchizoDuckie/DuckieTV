2b6da9203eb8f2c42cddcc4069911857c434746a
Garfield69 <Garfield69@outlook.com>
2016-03-20 07:10:30 +1300

    update xem service
    with a more uniform console logging.

--- js/services/SceneXemResolver.js
+++ js/services/SceneXemResolver.js
@@ -20,33 +20,32 @@ factory('SceneXemResolver', ["$q", "$http",
                 var lastFetched = ('xem.lastFetched' in localStorage) ? new Date(parseInt(localStorage.getItem('xem.lastFetched'))) : new Date();
                 if (('xem.mappings' in localStorage) && lastFetched.getTime() + 86400000 > new Date().getTime()) {
                     mappings = JSON.parse(localStorage.getItem('xem.mappings'));
-                    console.info("Fetched Xem mappings from localstorage: ", mappings);
+                    console.info("Fetched localstorage Xem series list: ", mappings);
                 } else {
-                    console.info("Updating Xem mapping list.");
                     $http.get('https://duckietv.github.io/xem-cache/mappings.json').then(function(response) {
                         mappings = response.data;
                         localStorage.setItem('xem.mappings', JSON.stringify(mappings));
                         localStorage.setItem('xem.lastFetched', new Date().getTime());
+                        console.info("Updating localstorage Xem series list:", mappings);
                     });
                 }
             },
             getEpisodeMapping: function(serie, episode, sceneName, append) {
                 if (mappings.indexOf(parseInt(serie.TVDB_ID)) > -1) {
                     return getXemCacheForSerie(serie.TVDB_ID).then(function(result) {
-                        //console.debug("Fetched Xross Entity Mapping for %s (%s)", serie.name, serie.TVDB_ID);
                         var matches = result.filter(function(show) {
                             return show.tvdb.season == episode.seasonnumber && show.tvdb.episode == episode.episodenumber;
                         });
                         if (matches.length > 0) {
-                            console.info("Returning Xross Entity Mapping mapping  for %s (%s)", serie.name, serie.TVDB_ID, matches[0].scene);
+                            console.info("Xem has episode for %s (%s), using mapped format.", serie.name, serie.TVDB_ID, matches[0].scene);
                             return sceneName + episode.formatEpisode(matches[0].scene.season, matches[0].scene.episode) + append;
                         } else {
-                            console.info("Episode not found in Xross Entity Map (TheXem.de) for %s (%s). Returning default formatting.", serie.name, serie.TVDB_ID);
+                            console.info("Xem does not have episode for %s (%s), using default format.", serie.name, serie.TVDB_ID);
                             return sceneName + episode.getFormattedEpisode() + append;
                         }
                     });
                 } else {
-                    console.info("returning default episode mapping from XEM because %s (%s) is not in the list", serie.name, serie.TVDB_ID);
+                    console.info("Xem does not have series %s (%s), using default format.", serie.name, serie.TVDB_ID);
                     return $q.resolve(sceneName + episode.getFormattedEpisode() + append);
                 }
             }
@@ -59,7 +58,7 @@ factory('SceneXemResolver', ["$q", "$http",
 .run(["SettingsService", "SceneXemResolver",
     function(SettingsService, SceneXemResolver) {
         if (SettingsService.get('torrenting.enabled')) {
-            console.info("Initializing Xross Scene Name Episode Mapping service");
+            console.info("Initializing Xross Entity Mapping (https://thexem.de) service for Scene Name episode format.");
             SceneXemResolver.initialize();
         }
     }
