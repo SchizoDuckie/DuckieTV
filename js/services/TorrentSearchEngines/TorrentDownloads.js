6c46d18a38c968d5389361dc30c652377f455b5f
Garfield69 <Garfield69@outlook.com>
2016-08-25 09:22:39 +1200

    add TorrentDownloads SE for #755

---
+++ js/services/TorrentSearchEngines/TorrentDownloads.js
@@ -0,0 +1,28 @@
+DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$injector",
+    function(TorrentSearchEngines, SettingsService, $q, $http, $injector) {
+        if (SettingsService.get('torrenting.enabled')) {
+            TorrentSearchEngines.registerSearchEngine('TorrentDownloads', new GenericTorrentSearchEngine({
+                mirror: 'https://www.torrentdownloads.me',
+                mirrorResolver: null,
+                includeBaseURL: true,
+                noMagnet: true,
+                endpoints: {
+                    search: '/search/?search=%s',
+                    details: '%s'
+                },
+                selectors: {
+                    resultContainer: 'div[class^="grey_bar3"]',
+                    releasename: ['p a[href^="/torrent/"]', 'innerText'],
+                    seeders: ['span:nth-of-type(3)', 'innerText'],
+                    leechers: ['span:nth-of-type(2)', 'innerText'],
+                    size: ['span:nth-of-type(4)', 'innerText'],
+                    detailUrl: ['p a[href^="/torrent/"]', 'href']
+                },
+                detailsSelectors: {
+                    detailsContainer: 'div[class="inner_container"]',
+                    magnetUrl: ['a[href^="magnet:"]', 'href']
+                }
+            }, $q, $http, $injector));
+        }
+    }
+]);
