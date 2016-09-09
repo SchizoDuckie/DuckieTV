f69bdf6641a640c855a49e73652379d9c8eea588
Js41637 ftw <js41637@gmail.com>
2016-09-09 13:42:36 +1000

    No badges or clicks on trakt-import posters

--- js/directives/serieheader.js
+++ js/directives/serieheader.js
@@ -2,22 +2,20 @@
  * Generic serie header directive
  * Displays a poster of a banner from a tv show and provides navigation to it via the template
  */
-DuckieTV.directive('serieheader', ["FavoritesService",
-    function(FavoritesService) {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: {
-                'data': '=data',
-                'noBadge': "=noBadge",
-                'noListButton': "=noButton",
-                "noOverview": "=noOverview",
-                "noTitle": "=noTitle",
-                "mode": "@"
-            },
-            templateUrl: function($node, $iAttrs) {
-                return $iAttrs.seriesList ? "templates/serieslist/serieHeader.html" : "templates/serieHeader.html";
-            },
-        };
-    }
-]);
+DuckieTV.directive('serieheader', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            'data': '=data',
+            'noBadge': "=noBadge",
+            'noListButton': "=noButton",
+            "noOverview": "=noOverview",
+            "noTitle": "=noTitle",
+            "mode": "@"
+        },
+        templateUrl: function($node, $iAttrs) {
+            return $iAttrs.seriesList ? "templates/serieslist/serieHeader.html" : "templates/serieHeader.html";
+        },
+    };
+});
