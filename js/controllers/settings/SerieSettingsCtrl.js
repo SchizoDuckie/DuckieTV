fc139434517afc8f969ffc92f35804dca0e363c8
Garfield69 <Garfield69@outlook.com>
2016-10-07 09:51:03 +1300

    better handling of local/remote dlPath #508

--- js/controllers/settings/SerieSettingsCtrl.js
+++ js/controllers/settings/SerieSettingsCtrl.js
@@ -7,15 +7,26 @@ function($scope, $filter, $modalInstance, FavoritesService, FormlyLoader, data, 
     $scope.model.ignoreGlobalQuality = $scope.model.ignoreGlobalQuality == 1;
     $scope.model.ignoreGlobalIncludes = $scope.model.ignoreGlobalIncludes == 1;
     $scope.model.ignoreGlobalExcludes = $scope.model.ignoreGlobalExcludes == 1;
-    if ((navigator.userAgent.toLowerCase().indexOf('standalone') !== -1)) {
-        var serieSettingsName = 'SerieSettingsStandalone';
-    } else {
-        var serieSettingsName = 'SerieSettings';
-    }
 
-    FormlyLoader.load(serieSettingsName).then(function(form) {
+    // determine if client is local or remote (not fool proof, is there a better way?)
+    if (DuckieTorrent.getClient().getName() === 'uTorrent') {
+        var server = ''; // uTorrent does not have a config.server
+    } else {
+        var server = DuckieTorrent.getClient().config.server;
+    }
+    var isLocal = (server === 'http://127.0.0.1' || server === 'http://localhost');
+    // determine if this is standalone
+    var isStandalone = (navigator.userAgent.toLowerCase().indexOf('standalone') !== -1);
+    // determine if downloadPath is supported by client
+    var isDownloadPathSupported = DuckieTorrent.getClient().isDownloadPathSupported();
+    $scope.model.dlPathLocal = $scope.model.dlPath;
+    $scope.model.dlPathRemote = $scope.model.dlPath;
+
+    FormlyLoader.load('SerieSettings').then(function(form) {
         $scope.fields = form;
-        $scope.model.isDownloadPathSupported = DuckieTorrent.getClient().isDownloadPathSupported();
+        // don't understand why this is here, other than the fact that if it is not then the hideExpressions don't work :-( 
+        $scope.model.isDownloadPathSupportedLocal = (isDownloadPathSupported && isStandalone && isLocal);
+        $scope.model.isDownloadPathSupportedRemote = (isDownloadPathSupported && ((!isStandalone) || (isStandalone && !isLocal)));
     });
 
     $scope.searchProviders = [{'name': '', 'value': null}];
@@ -36,7 +47,14 @@ function($scope, $filter, $modalInstance, FavoritesService, FormlyLoader, data, 
         // despite (because?) type=number, some invalid data trapped by formly returns undefined. so this ensures that we persist as null to stop downstream errors.
         $scope.model.customSearchSizeMin = (typeof $scope.model.customSearchSizeMin === 'undefined') ? null : $scope.model.customSearchSizeMin;
         $scope.model.customSearchSizeMax = (typeof $scope.model.customSearchSizeMax === 'undefined') ? null : $scope.model.customSearchSizeMax;
-        $scope.model.dlPath = (typeof $scope.model.dlPath === 'undefined' || $scope.model.dlPath === '') ? null : $scope.model.dlPath;
+        if ($scope.model.isDownloadPathSupportedLocal) {
+            // save model dlPath from content of model dlPathLocal
+            $scope.model.dlPath = (typeof $scope.model.dlPathLocal === 'undefined' || $scope.model.dlPathLocal === '') ? null : $scope.model.dlPathLocal;
+        }
+        if ($scope.model.isDownloadPathSupportedRemote) {
+            // save model dlPath from content of model dlPathRemote
+            $scope.model.dlPath = (typeof $scope.model.dlPathRemote === 'undefined' || $scope.model.dlPathRemote === '') ? null : $scope.model.dlPathRemote;
+        }
 
         $scope.model.Persist().then(function() {
             $modalInstance.close();
