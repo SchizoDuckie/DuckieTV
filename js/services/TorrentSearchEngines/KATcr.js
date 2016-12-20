c1e8fb98f418040a4636abdaac1ce2f2aab0cb18
Garfield69 <Garfield69@outlook.com>
2016-12-20 19:22:31 +1300

    add KATcr and TorrentZ2 SE

---
+++ js/services/TorrentSearchEngines/KATcr.js
@@ -0,0 +1,35 @@
+DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$injector",
+    function(TorrentSearchEngines, SettingsService, $q, $http, $injector) {
+        if (SettingsService.get('torrenting.enabled')) {
+            TorrentSearchEngines.registerSearchEngine('KATcr', new GenericTorrentSearchEngine({
+                mirror: 'https://katcr.co/new/',
+                mirrorResolver: null,
+                includeBaseURL: true,
+                noMagnet: true,
+                endpoints: {
+                    search: 'torrents-search.php?search=%s&sort=%o',
+                    details: '%s'
+                },
+                selectors: {
+                    resultContainer: 'tr[class="t-row"]',
+                    releasename: ['td:first-child div.torrentname div a.cellMainLink', 'innerText'],
+                    size: ['td:nth-child(2)', 'innerText'],
+                    seeders: ['td:nth-child(4)', 'innerText'],
+                    leechers: ['td:nth-child(5)', 'innerText'],
+                    detailUrl: ['td:first-child div[class^="iaconbox"] a:nth-child(2)', 'href']
+                },
+                detailsSelectors: {
+                    detailsContainer: 'table tr td div[class="myFrame torrent-title"]',
+                    magnetUrl: ['a[title="Magnet link"]', 'href']
+                },
+                orderby: {
+                    age: {d: 'id&order=desc', a: 'id&order=asc'},
+                    seeders: {d: 'seeders&order=desc', a: 'seeders&order=asc'},
+                    leechers: {d: 'leechers&order=desc', a: 'leechers&order=asc'},
+                    size: {d: 'size&order=desc', a: 'size&order=asc'}
+                }
+            }, $q, $http, $injector));
+        }
+
+    }
+]);
---
+++ js/services/TorrentSearchEngines/TorrentZ2.js
@@ -0,0 +1,36 @@
+DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$injector",
+    function(TorrentSearchEngines, SettingsService, $q, $http, $injector) {
+        if (SettingsService.get('torrenting.enabled')) {
+
+            TorrentSearchEngines.registerSearchEngine('TorrentZ2', new GenericTorrentSearchEngine({
+                mirror: 'https://torrentz2.eu',
+                mirrorResolver: null,
+                includeBaseURL: true,
+                endpoints: {
+                    search: '/search%o?f=%s',
+                    details: '%s'
+                },
+                selectors: {
+                    resultContainer: 'div.results dl',
+                    releasename: ['dt a', 'innerText'],
+                    magnetUrl: ['dt a', 'href',
+                        function(href) {
+                            return 'magnet:?xt=urn:btih:' + href.substring(1) + TorrentSearchEngines.trackers;
+                        }
+                    ],
+                    size: ['dd span:nth-child(3)', 'innerText'],
+                    seeders: ['dd span:nth-child(4)', 'innerText'],
+                    leechers: ['dd span:nth-child(5)', 'innerText'],
+                    detailUrl: ['dt a', 'href']
+                },
+                noDetailsMagnet: true,
+                orderby: {
+                    age: {d: 'A', a: 'A'},
+                    seeders: {d: '', a: ''},
+                    size: {d: 'S', a: 'S'}
+                }
+
+            }, $q, $http, $injector));
+        }
+    }
+]);
