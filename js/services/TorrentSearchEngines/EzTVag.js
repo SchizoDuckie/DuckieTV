fd3692313626d8e24f0bead4efbd5bf091ef7b0a
SchizoDuckie <schizoduckie@gmail.com>
2017-02-07 20:24:39 +0100

    eztv.ag support as requested on readdit

---
+++ js/services/TorrentSearchEngines/EzTVag.js
@@ -0,0 +1,26 @@
+DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$injector",
+    function(TorrentSearchEngines, SettingsService, $q, $http, $injector) {
+        if (SettingsService.get('torrenting.enabled')) {
+
+            TorrentSearchEngines.registerSearchEngine('EzTV.ag', new GenericTorrentSearchEngine({
+                mirror: 'https://eztv.ag/',
+                mirrorResolver: null,
+                includeBaseURL: true,
+                endpoints: {
+                    search: 'search/%s'
+                },
+                selectors: {
+                    resultContainer: 'table.forum_header_border tr.forum_header_border',
+                    releasename: ['td > a.epinfo', 'innerText'],
+                    magnetUrl: ['td > a.magnet', 'href'],
+                    size: ['td:nth-child(4)', 'innerText'],
+                    seeders: ['td:nth-child(6)', 'innerText'],
+                    leechers: ['td:nth-child(6)', 'innerText', function(a) {
+                        return '?';
+                    }],
+                    detailUrl: ['td.forum_thread_post > a.epinfo', 'href']
+                }
+            }, $q, $http, $injector));
+        }
+    }
+]);
