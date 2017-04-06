140e1de2763a170f037e8af579d8bc18e8223754
Garfield69 <Garfield69@outlook.com>
2017-04-06 16:12:05 +1200

    restore switch

--- js/services/TorrentSearchEngines/Idope.js
+++ js/services/TorrentSearchEngines/Idope.js
@@ -1,43 +1,44 @@
 DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$injector",
     function(TorrentSearchEngines, SettingsService, $q, $http, $injector) {
+        if (SettingsService.get('torrenting.enabled')) {
 
-        TorrentSearchEngines.registerSearchEngine('Idope', new GenericTorrentSearchEngine({
-            mirror: 'https://idope.se',
-            mirrorResolver: null,
-            includeBaseURL: true,
-            endpoints: {
-                search: '/torrent-list/%s/?&o=%o'
-            },
-            selectors: {
-                resultContainer: 'div.resultdiv',
-                releasename: ['div.resultdivtopname', 'innerText'],
-                seeders: ['div.resultdivbottonseed', 'innerText'],
-                leechers: ['div.resultdivbottonseed', 'innerText',
-                    function(text) {
-                        return 'n/a';
-                    }
-                ],
-                size: ['div.resultdivbottonlength', 'innerText'],
-                detailUrl: ['div.resultdivtop a', 'href'],
-                magnetUrl: ['.hideinfohash', 'innerText',
-                    function(href) {
-                        var magnetHash = href.match(/([0-9ABCDEFabcdef]{40})/);
-                        return 'magnet:?xt=urn:btih:' + magnetHash[0] + $injector.get('TorrentSearchEngines').trackers;
-                    }
-                ]
-            },
-            orderby: {
-                seeders: {
-                    d: '-1',
-                    a: '1'
+            TorrentSearchEngines.registerSearchEngine('Idope', new GenericTorrentSearchEngine({
+                mirror: 'https://idope.se',
+                mirrorResolver: null,
+                includeBaseURL: true,
+                endpoints: {
+                    search: '/torrent-list/%s/?&o=%o'
                 },
-                size: {
-                    d: '-2',
-                    a: '2'
+                selectors: {
+                    resultContainer: 'div.resultdiv',
+                    releasename: ['div.resultdivtopname', 'innerText'],
+                    seeders: ['div.resultdivbottonseed', 'innerText'],
+                    leechers: ['div.resultdivbottonseed', 'innerText',
+                        function(text) {
+                            return 'n/a';
+                        }
+                    ],
+                    size: ['div.resultdivbottonlength', 'innerText'],
+                    detailUrl: ['div.resultdivtop a', 'href'],
+                    magnetUrl: ['.hideinfohash', 'innerText',
+                        function(href) {
+                            var magnetHash = href.match(/([0-9ABCDEFabcdef]{40})/);
+                            return 'magnet:?xt=urn:btih:' + magnetHash[0] + $injector.get('TorrentSearchEngines').trackers;
+                        }
+                    ]
+                },
+                orderby: {
+                    seeders: {
+                        d: '-1',
+                        a: '1'
+                    },
+                    size: {
+                        d: '-2',
+                        a: '2'
+                    }
                 }
-            }
-        }, $q, $http, $injector));
-
+            }, $q, $http, $injector));
+        }
 
     }
 ]);
