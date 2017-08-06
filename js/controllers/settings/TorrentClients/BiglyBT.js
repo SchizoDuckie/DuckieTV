73843af7dff69d66de8db7a4d63a675dda44f983
Garfield69 <Garfield69@outlook.com>
2017-08-06 19:41:52 +1200

    BiglyBT client
    Identical to Vuze, built on Transmission API, so easy to implement ;-)

---
+++ js/controllers/settings/TorrentClients/BiglyBT.js
@@ -0,0 +1,43 @@
+DuckieTV.controller("biglybtCtrl", ["$injector", "BiglyBT", "SettingsService", "FormlyLoader",
+    function($injector, BiglyBT, SettingsService, FormlyLoader) {
+
+        var self = this;
+        this.error = null;
+
+        FormlyLoader.load('TorrentClientSettings').then(function(fields) {
+
+            self.model = {
+                server: SettingsService.get('biglybt.server'),
+                port: SettingsService.get('biglybt.port'),
+                path: SettingsService.get('biglybt.path'),
+                use_auth: SettingsService.get('biglybt.use_auth'),
+                username: SettingsService.get('biglybt.username'),
+                password: SettingsService.get('biglybt.password'),
+                hidePath: true
+            };
+
+            self.fields = fields;
+        });
+
+        this.isConnected = function() {
+            return BiglyBT.isConnected();
+        };
+
+
+        this.test = function() {
+            this.error = false;
+            //console.log("Testing settings");
+            BiglyBT.Disconnect();
+            BiglyBT.setConfig(this.model);
+            BiglyBT.connect().then(function(connected) {
+                console.info("BiglyBT connected! (save settings)", connected);
+                self.error = null;
+                BiglyBT.saveConfig();
+                window.location.reload();
+            }, function(error) {
+                self.error = error;
+                console.error("BiglyBT connect error!", error);
+            });
+        };
+    }
+]);
---
+++ js/services/TorrentClients/BiglyBT.js
@@ -0,0 +1,37 @@
+/**
+ * BiglyBT has *exactly* the same API as Transmission, so we'll just use that whole implementation and change the config
+ * it reads from.
+ */
+DuckieTorrent.factory('BiglyBT', ["BaseTorrentClient", "TransmissionRemote", "TransmissionAPI",
+    function(BaseTorrentClient, TransmissionRemote, TransmissionAPI) {
+
+        var BiglyBT = function() {
+            BaseTorrentClient.call(this);
+        };
+        BiglyBT.extends(BaseTorrentClient, {});
+
+        var service = new BiglyBT();
+        service.setName('BiglyBT');
+        service.setAPI(new TransmissionAPI());
+        service.setRemote(new TransmissionRemote());
+        service.setConfigMappings({
+            server: 'biglybt.server',
+            port: 'biglybt.port',
+            path: 'biglybt.path',
+            username: 'biglybt.username',
+            password: 'biglybt.password',
+            use_auth: 'biglybt.use_auth'
+        });
+        service.readConfig();
+
+        return service;
+    }
+])
+
+.run(["DuckieTorrent", "BiglyBT", "SettingsService",
+    function(DuckieTorrent, BiglyBT, SettingsService) {
+        if (SettingsService.get('torrenting.enabled')) {
+            DuckieTorrent.register('BiglyBT', BiglyBT);
+        }
+    }
+]);
