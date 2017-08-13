41780fb61944dba41a0dc896da0ef1ccc5101100
Garfield69 <Garfield69@outlook.com>
2017-08-14 10:25:58 +1200

    refresh fanart when using seriesDetails refresh button #937

--- js/services/FavoritesManager.js
+++ js/services/FavoritesManager.js
@@ -17,7 +17,7 @@ DuckieTV.factory("FavoritesManager", ["FavoritesService", "TraktTVv2", "$rootSco
                 FavoritesService.adding(serie.tvdb_id);
                 var id = serie.trakt_id || serie.imdb_id || serie.slug_id;
                 return TraktTVv2.serie(id).then(function(serie) {
-                    return FavoritesService.addFavorite(serie).then(function() {
+                    return FavoritesService.addFavorite(serie, undefined, true, true).then(function() {
                         $rootScope.$broadcast('storage:update');
                         FavoritesService.added(serie.tvdb_id);
                         return true;
