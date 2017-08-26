c8738d6be25c8fd095ee4bfaf13f7e2f9894041d
Garfield69 <Garfield69@outlook.com>
2017-08-26 20:55:40 +1200

    UNS changelog

--- js/services/UpgradeNotificationService.js
+++ js/services/UpgradeNotificationService.js
@@ -14,12 +14,14 @@ DuckieTV
             '1.1.5': ["<li>AutoDownload: (new) Now able to download .torrent files \o/.",
                 "<li>Database: (fix) Moved the database management services to the *Background* task, to minimise interruptions to DB writes and maintain integrity.",
                 "<li>Favourites: (new) Remembers last used sort selection.",
+                "<li>Favourites: (new) Introducing Anime support. Now search uses the absolute episode number when available. Series Settings allows user to select any available Alias to be used in searches instead of the default Title.",
                 "<li>SearchEngines: Settings for Jackett! You can use the Jackett proxy to access your favourite open/semi-private/private trackers as DuckieTV Search Engines.",
                 "<li>Standalone: (upgrade) nwjs 24.2 chromium 60 Node 8.3.0",
                 "<li>TorrentClient: (new) Added BiglyBT client.",
                 "<li>TorrentClient: (fix) DuckieTV can now connect with qBitTorrent 3.3.14 (and newer) with CSRF protection.",
                 "<li>TorrentClient: (fix) DuckieTV can now connect with Deluge 1.3.14 (and newer) with CSRF protection.",
                 "<li>Torrent Dialog [multi-SE]: (new) Remembers last used sort selection.",
+                "<li>Trakt-Sync: (fix) DuckieTV now adding a downloaded episode to the user's trakt account collected list.",
                 "<li>Trakt-Trending sidepanel: (upgrade) Now using buttons instead of mouse-hover to improve browsing experience.",
                 "<li>Misc bug fixes."
             ].join('')
