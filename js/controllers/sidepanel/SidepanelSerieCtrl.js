2c8ca22fa145c4b3cad0dc7c6baefb1df2bfedba
Garfield69 <Garfield69@outlook.com>
2018-03-22 07:44:45 +1300

    sidepanelSerieCtrl: add missing closeSidePanelExpansion #1061

--- js/controllers/sidepanel/SidepanelSerieCtrl.js
+++ js/controllers/sidepanel/SidepanelSerieCtrl.js
@@ -1,5 +1,5 @@
-DuckieTV.controller('SidepanelSerieCtrl', ["$rootScope", "$filter", "$location", "$locale", "$q", "$state", "dialogs", "FavoritesService", "latestSeason", "notWatchedSeason", "serie", "SidePanelState", "SettingsService", "FavoritesManager",
-    function($rootScope, $filter, $location, $locale, $q, $state, dialogs, FavoritesService, latestSeason, notWatchedSeason, serie, SidePanelState, SettingsService, FavoritesManager) {
+DuckieTV.controller('SidepanelSerieCtrl', ["$rootScope", "$filter", "$location", "$locale", "$q", "$state", "$injector", "dialogs", "FavoritesService", "latestSeason", "notWatchedSeason", "serie", "SidePanelState", "SettingsService", "FavoritesManager",
+    function($rootScope, $filter, $location, $locale, $q, $state, $injector, dialogs, FavoritesService, latestSeason, notWatchedSeason, serie, SidePanelState, SettingsService, FavoritesManager) {
 
         var self = this;
         this.serie = serie;
@@ -15,6 +15,13 @@ DuckieTV.controller('SidepanelSerieCtrl', ["$rootScope", "$filter", "$location",
         this.closeSidePanel = function() {
             SidePanelState.hide();
         };
+        /**
+         * Closes the SidePanel expansion
+         */
+        this.closeSidePanelExpansion = function() {
+            $injector.get('SidePanelState').contract();
+            $state.go('serie');
+        }
 
         this.refresh = function(serie) {
             this.isRefreshing = true;
@@ -153,6 +160,11 @@ DuckieTV.controller('SidepanelSerieCtrl', ["$rootScope", "$filter", "$location",
          */
         this.isAdding = function(tvdb_id) {
             return FavoritesService.isAdding(tvdb_id);
+        };
+
+        this.dataToClipboard = function(data) {
+            var clip = nw.Clipboard.get();
+            clip.set(data.replace(/\|/g,'\r\n'), 'text');
         };
     }
 ]);
