eba047b9de8c393264df820ed0e6b4a354da6c87
Js41637 <Js41637@users.noreply.github.com>
2018-09-19 20:32:48 +1000

    Remove need for two duplicate serieHeaders
    I don't think two identical serieHeaders are needed?? @garfield69

--- js/directives/serieheader.js
+++ js/directives/serieheader.js
@@ -2,27 +2,25 @@
  * Generic serie header directive
  * Displays a poster of a banner from a tv show and provides navigation to it via the template
  */
-DuckieTV.directive('serieheader', ['FanartService', function(FanartService) {
-    return {
-        restrict: 'E',
-        transclude: true,
-        scope: {
-            'data': '=data',
-            'noBadge': "=noBadge",
-            'noListButton': "=noButton",
-            "noOverview": "=noOverview",
-            "noTitle": "=noTitle",
-            "mode": "@"
-        },
-        templateUrl: function($node, $iAttrs) {
-            return $iAttrs.seriesList ? "templates/serieslist/serieHeader.html" : "templates/serieHeader.html";
-        },
-        controller: ["$element","$attrs", "$scope", function($element, $attrs, $scope) {
-            if(!$scope.data.poster) {
-                FanartService.get($scope.data.tvdb_id).then(function(found){
-                    $scope.data.poster = found.poster
-                })
-            }
-        }]
-    };
+DuckieTV.directive('serieheader', ['FanartService', function (FanartService) {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      'data': '=data',
+      'noBadge': "=noBadge",
+      'noListButton': "=noButton",
+      "noOverview": "=noOverview",
+      "noTitle": "=noTitle",
+      "mode": "@"
+    },
+    templateUrl: "templates/serieHeader.html",
+    controller: ["$element", "$attrs", "$scope", function ($element, $attrs, $scope) {
+      if(!$scope.data.poster) {
+        FanartService.get($scope.data.tvdb_id).then(function (found) {
+          $scope.data.poster = found.poster;
+        });
+      }
+    }]
+  };
 }]);
