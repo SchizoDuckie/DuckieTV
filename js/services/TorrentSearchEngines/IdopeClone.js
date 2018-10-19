16b83aa8715444ac7cd6ef10d612a9d53acee50e
Garfield69 <garfield69@outlook.com>
2018-10-19 16:34:21 +1300

    idopeclone SE: new domain
    plus now returning seeds

--- js/services/TorrentSearchEngines/IdopeClone.js
+++ js/services/TorrentSearchEngines/IdopeClone.js
@@ -3,32 +3,28 @@ DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$inject
         if (SettingsService.get('torrenting.enabled')) {
 
             TorrentSearchEngines.registerSearchEngine('IdopeClone', new GenericTorrentSearchEngine({
-                mirror: 'https://idope.top',
+                mirror: 'https://www.idope.site',
                 mirrorResolver: null,
                 includeBaseURL: false,
                 endpoints: {
                     search: '/search/%s/'
                 },
                 selectors: {
-                    resultContainer: 'div.resultdiv',
-                    releasename: ['div.resultdivtopname', 'innerText'],
-                    detailUrl: ['div.magneticdiv a', 'href'],
-                    seeders: ['div.resultdivbottonseed', 'innerText',
+                    resultContainer: 'li',
+                    releasename: ['div.opt-text-w3layouts a', 'innerText'],
+                    detailUrl: ['div.opt-text-w3layouts a', 'href'],
+                    seeders: ['div.seedbar span:nth-child(1)', 'innerText',
+                        function(text) {
+                            return text.replace('Seed: ', '');
+                        }
+                    ],
+                    leechers: ['div.seedbar span:nth-child(1)', 'innerText',
                         function(text) {
                             return null;
                         }
                     ],
-                    leechers: ['div.resultdivbottonseed', 'innerText',
-                        function(text) {
-                            return null;
-                        }
-                    ],
-                    size: ['div.resultdivbottonlength', 'innerText'],
-                    magnetUrl: ['div.resultdivbottonseed', 'innerText',
-                        function(text) {
-                            return 'magnet:?xt=urn:btih:' + text + TorrentSearchEngines.trackers;
-                        }
-                    ]
+                    size: ['div.seedbar span:nth-child(2)', 'innerText'],
+                    magnetUrl: ['div.opt-text-w3layouts a', 'href']
                 }
             }, $q, $http, $injector));
         }
