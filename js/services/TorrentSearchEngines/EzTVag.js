dcb74e26c031457568dd98ee93cd7cc006855b45
Garfield69 <garfield69@outlook.com>
2018-11-23 14:07:53 +1300

    eztv: new domain .io

--- js/services/TorrentSearchEngines/EzTVag.js
+++ js/services/TorrentSearchEngines/EzTVag.js
@@ -3,7 +3,7 @@ DuckieTV.run(["TorrentSearchEngines", "SettingsService", "$q", "$http", "$inject
         if (SettingsService.get('torrenting.enabled')) {
 
             TorrentSearchEngines.registerSearchEngine('EzTV.ag', new GenericTorrentSearchEngine({
-                mirror: 'https://eztv.re/',
+                mirror: 'https://eztv.io/',
                 mirrorResolver: null,
                 includeBaseURL: true,
                 endpoints: {
