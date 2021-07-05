6691f3ed73b8e56c9f11d91581b8410a927c6665
Garfield69 <garfield69@outlook.com>
2021-07-06 09:04:39 +1200

    thepiratebaymirrorresolver: new domain *.com

--- js/services/TorrentSearchEngines/ThePirateBayMirrorResolver.js
+++ js/services/TorrentSearchEngines/ThePirateBayMirrorResolver.js
@@ -8,7 +8,7 @@ DuckieTV.factory('ThePirateBayMirrorResolver', ['$q', '$http', '$injector',
     var maxAttempts = 3
 
     var endpoints = {
-      thepiratebay: 'https://proxy-bay.co/'
+      thepiratebay: 'https://proxy-bay.com/'
     }
 
     /**
