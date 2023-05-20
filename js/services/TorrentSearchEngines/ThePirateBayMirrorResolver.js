1a23c9af1f852ed49e926623daac53c0f233e6ae
Garfield69 <garfield69@outlook.com>
2023-05-21 06:40:40 +1200

    Update ThePirateBayMirrorResolver.js

--- js/services/TorrentSearchEngines/ThePirateBayMirrorResolver.js
+++ js/services/TorrentSearchEngines/ThePirateBayMirrorResolver.js
@@ -1,5 +1,5 @@
 /**
- * Automatic mirror resolver for ThePirateBay by utilizing proxy-bay.app
+ * Automatic mirror resolver for ThePirateBay by utilizing proxy-bay.click
  */
 DuckieTV.factory('ThePirateBayMirrorResolver', ['$q', '$http', '$injector',
   function($q, $http, $injector) {
@@ -8,7 +8,7 @@ DuckieTV.factory('ThePirateBayMirrorResolver', ['$q', '$http', '$injector',
     var maxAttempts = 3
 
     var endpoints = {
-      thepiratebay: 'https://proxy-bay.ink/'
+      thepiratebay: 'https://proxy-bay.click/'
     }
 
     /**
