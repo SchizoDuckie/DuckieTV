28248a9066f43bb312a76a7e46abd072c4d91a40
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-01-25 07:41:08 +0000

    Update MigrationService.js

--- js/services/MigrationService.js
+++ js/services/MigrationService.js
@@ -30,7 +30,7 @@ DuckieTV.run(['SettingsService', function(SettingsService) {
   // update quality list
   if (!localStorage.getItem('1.1.6updateQualityList')) {
     console.info('Executing 1.1.6updateQualityList')
-    SettingsService.set('torrenting.searchqualitylist', ['HDTV', '720p', '1080p', '2160p', 'x265']);
+    SettingsService.set('torrenting.searchqualitylist', ['HDTV', 'WEB', '720p', '1080p', '2160p', 'x265']);
     localStorage.setItem('1.1.6updateQualityList', new Date())
     console.info('1.1.6updateQualityList done!')
   }
