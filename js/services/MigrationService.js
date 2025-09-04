b03006624c646455432dda1fd3fdfb411618de1a
Garfield69 <garfield69@outlook.com>
2025-09-05 07:14:59 +1200

    migrationservice and readme update

--- js/services/MigrationService.js
+++ js/services/MigrationService.js
@@ -44,4 +44,26 @@ DuckieTV.run(['SettingsService', function(SettingsService) {
     localStorage.setItem('1.1.6updateqBt32+toqBt41+', new Date())
     console.info('1.1.6updateqBt32+toqBt41+ done!')
   }
+  // SE domain updates (for those that have used a backup restore)
+  if (!localStorage.getItem('1.1.6SEdomainUpdate')) {
+    console.info('Executing 1.1.6SEdomainUpdate')
+    SettingsService.set('mirror.1337x', 'https://1337x.to');
+    SettingsService.set('mirror.ETag', 'https://extratorrent.st');
+    SettingsService.set('mirror.EXT', 'https://ext.to');
+    SettingsService.set('mirror.FileMood', 'https://filemood.com');
+    SettingsService.set('mirror.Idope', 'https://idope.se');
+    SettingsService.set('mirror.IsoHunt2', 'https://isohunt.ch');
+    SettingsService.set('mirror.KATws', 'https://kickass.ws');
+    SettingsService.set('mirror.Knaben', 'https://knaben.org');
+    SettingsService.set('mirror.LimeTorrents', 'https://www.limetorrents.fun');
+    SettingsService.set('mirror.Nyaa', 'https://nyaa.si');
+    SettingsService.set('mirror.PiratesParadise', 'https://piratesparadise.org');
+    SettingsService.set('mirror.ShowRSS', 'https://showrss.info');
+    SettingsService.set('mirror.theRARBG', 'https://therarbg.to');
+    SettingsService.set('mirror.ThePirateBay', 'https://thepiratebay0.org/');
+    SettingsService.set('mirror.TorrentDownloads', 'https://www.torrentdownloads.pro');
+    SettingsService.set('mirror.Uindex', 'https://uindex.org');
+    localStorage.setItem('1.1.6SEdomainUpdate', new Date())
+    console.info('1.1.6SEdomainUpdate done!')
+  }
 }])
